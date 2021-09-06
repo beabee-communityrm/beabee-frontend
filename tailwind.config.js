d03821b03647ce38ef72e7de770e55fe20ef7117
Poriya <poriya.ap@pm.me>
2021-09-06 17:56:09 +0300

    feat: add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,37 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        light: 'var(--primary-light-color)',
+        DEFAULT: 'var(--primary-color)',
+        dark: 'var(--primary-dark-color)',
+      },
+      success: {
+        light: 'var(--success-light-color)',
+        DEFAULT: 'var(--success-color)',
+        dark: 'var(--success-dark-color)',
+      },
+      danger: {
+        light: 'var(--danger-light-color)',
+        DEFAULT: 'var(--danger-color)',
+        dark: 'var(--danger-dark-color)',
+      },
+    },
+    textColor: {
+      dark: {
+        100: 'var(--secondary-dark-text-color)',
+        DEFAULT: 'var(--primary-dark-text-color)',
+      },
+      white: {
+        DEFAULT: 'var(--primary-white-text-color)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        body: 'var(--body-font)',
+      },
+    },
   },
   variants: {
     extend: {},
