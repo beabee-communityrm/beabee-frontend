954324ef378f3ccb089bd4dc5d3d863e5b978736
Poriya <poriya.ap@pm.me>
2021-09-16 19:09:53 +0300

    feat: add i18n to menu

--- src/layouts/menu/menu-list.ts
+++ src/layouts/menu/menu-list.ts
@@ -1,31 +1,34 @@
+import i18n from '../../i18n';
+const { t } = i18n.global;
+
 export default [
   {
-    title: 'Home',
+    title: t('menu.home'),
     href: '/profile',
     icon: 'building',
   },
   {
-    title: 'Your profile',
+    title: t('menu.yourProfile'),
     href: '/profile/edit',
     icon: 'user',
   },
   {
-    title: 'Callouts',
+    title: t('menu.callouts'),
     href: '/callouts',
     icon: 'calendar-check',
   },
   {
-    title: 'Settings',
+    title: t('menu.settings'),
     divider: true,
   },
   {
-    title: 'Your information',
+    title: t('menu.yourInformation'),
     href: '/profile/information',
     icon: 'address-card',
     isSubsection: true,
   },
   {
-    title: 'Contributions',
+    title: t('menu.contributions'),
     href: '/profile/contributions',
     icon: 'credit-card',
     isSubsection: true,
