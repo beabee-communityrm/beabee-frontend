b3b88bbd6d1e31bf25226d6de9023bae3bb6c01c
porivo <poriya.ap@pm.me>
2021-10-21 15:53:14 +0300

    chore: add i18n for QuickActions component

--- src/modules/home/QuickActions.vue
+++ src/modules/home/QuickActions.vue
@@ -1,19 +1,30 @@
 <template>
-  <SectionTitle>QUICK ACTIONS</SectionTitle>
+  <SectionTitle class="uppercase">{{
+    t('homePage.quickActions')
+  }}</SectionTitle>
 
   <ul class="list-disc pl-4 font-semibold underline space-y-2 mt-4">
     <li>
-      <router-link to="/profile/edit">Edit your community profile</router-link>
+      <router-link to="/profile/edit">
+        {{ t('homePage.editProfile') }}
+      </router-link>
     </li>
     <li>
-      <router-link to="/callouts">View open callouts</router-link>
+      <router-link to="/callouts">
+        {{ t('homePage.viewOpenCallouts') }}
+      </router-link>
     </li>
     <li>
-      <router-link to="/contributions">Manage your payments</router-link>
+      <router-link to="/contributions">
+        {{ t('homePage.managePayment') }}
+      </router-link>
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
 import SectionTitle from './SectionTitle.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
