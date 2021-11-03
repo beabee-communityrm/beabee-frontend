7ce3458dddf921e0407fcb497e237b08835d666b
Will Franklin <will@willpf.co.uk>
2021-11-03 14:11:10 +0000

    feat: support locales with variants

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,25 @@
 import { createI18n } from 'vue-i18n';
-import messages from '@intlify/vite-plugin-vue-i18n/messages';
+
+const localeWithVariant = import.meta.env.VITE_LOCALE as string;
+const messages = await import(`../locales/${localeWithVariant}.json`);
+const [locale] = localeWithVariant.split('@');
+
+// We don't allow changing locales for now so fine to hardcode
 
 const numberFormats = {
-  en: {
-    currency: {
-      style: 'currency',
-      currency: import.meta.env.VITE_CURRENCY as string,
-    },
-  },
-  de: {
+  [locale]: {
     currency: {
       style: 'currency',
       currency: import.meta.env.VITE_CURRENCY as string,
     },
   },
 };
+
 const i18n = createI18n({
-  locale: 'en',
-  messages,
+  locale,
+  messages: {
+    [locale]: messages.default,
+  },
   numberFormats,
 });
 
