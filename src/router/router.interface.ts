ab491b9da1f7782e3f545b1a798313d929fd5afa
porivo <poriya.ap@pm.me>
2021-11-30 15:29:19 +0300

    fix: make roles required in route meta

--- src/router/router.interface.ts
+++ src/router/router.interface.ts
@@ -6,6 +6,6 @@ declare module 'vue-router' {
     pageTitle: string;
     layout?: string;
     // if a route has an empty roles array it means it can be accsesed by all users
-    roles?: Roles[] | [];
+    roles: Roles[] | [];
   }
 }
