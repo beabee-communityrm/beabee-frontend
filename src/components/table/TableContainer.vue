f90be4c82ba9560635ae1f9e4b3927edc7909443
porivo <poriya.ap@pm.me>
2021-12-01 16:25:02 +0300

    chore(wip): add initial part of table component

---
+++ src/components/table/TableContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(item, index) in headers" :key="index" align="left">
+          {{ item.text }}
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(item, i) in items" :key="i">
+        <td v-for="(data, j) in item" :key="j">{{ data }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  headers: {
+    type: Array,
+    default: () => [],
+  },
+  items: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
