1ce35ccb863d84a6b71f7d4b79e7594c512b90c4
porivo <poriya.ap@pm.me>
2021-12-01 19:56:10 +0300

    chore(wip): add more style

--- src/components/table/TableContainer.vue
+++ src/components/table/TableContainer.vue
@@ -1,16 +1,26 @@
 <template>
-  <table>
-    <thead>
+  <table class="font-semibold">
+    <thead class="text-sm border-b border-primary-20">
       <tr>
-        <th v-for="(item, index) in headers" :key="index" align="left">
+        <th
+          v-for="(item, index) in headers"
+          :key="index"
+          class="pb-2"
+          align="left"
+          :style="{ width: item.width }"
+        >
           {{ item.text }}
         </th>
       </tr>
     </thead>
 
-    <tbody>
-      <tr v-for="(item, i) in items" :key="i">
-        <td v-for="(value, key, j) in item" :key="j">
+    <tbody class="text-xs">
+      <tr
+        v-for="(item, i) in items"
+        :key="i"
+        class="border-b border-primary-20"
+      >
+        <td v-for="(value, key, j) in item" :key="j" class="py-2 align-top">
           <slot :name="key" :item="item">{{ value }}</slot>
         </td>
       </tr>
@@ -19,6 +29,12 @@
 </template>
 
 <script lang="ts" setup>
+// custom tabelcell:
+// `email` should be replaced with the object property name of item
+//     <template #email="{ item }">
+//      <span class="text-secondary">{{ item.email }}</span>
+//    </template>
+
 defineProps({
   headers: {
     type: Array,
