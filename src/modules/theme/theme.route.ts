59f330259a1fbbf7a90a660e69e52c773c18b1f8
Will Franklin <will@willpf.co.uk>
2022-01-06 15:40:14 +0000

    chore: make /_theme accessible to all

--- src/modules/theme/theme.route.ts
+++ src/modules/theme/theme.route.ts
@@ -1,9 +1,14 @@
 import { RouteRecordRaw } from 'vue-router';
+import { Role } from '../../utils/enums/roles.enum';
 
 export const themeRoute: Array<RouteRecordRaw> = [
   {
     path: '/_theme',
     name: 'theme',
+    meta: {
+      roles: [Role.NotLoggedIn],
+      pageTitle: 'Theme settings',
+    },
     component: () => import('./theme.vue'),
   },
 ];
