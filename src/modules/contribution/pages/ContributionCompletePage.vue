6449edede5743831b6eb322d4732f98c6d6b41bb
Will Franklin <will@willpf.co.uk>
2022-01-21 15:29:52 +0000

    feat: add loading pages for contributon changes

---
+++ src/modules/contribution/pages/ContributionCompletePage.vue
@@ -0,0 +1,21 @@
+<template><div /></template>
+
+<script lang="ts" setup>
+import { onBeforeMount } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { completeStartContribution } from '../../../utils/api/member';
+
+const route = useRoute();
+const router = useRouter();
+
+onBeforeMount(() => {
+  completeStartContribution(route.query.redirect_flow_id as string)
+    .then(() => {
+      router.replace({
+        path: '/profile/contribution',
+        query: { startedContribution: null },
+      });
+    })
+    .catch((err) => err);
+});
+</script>
