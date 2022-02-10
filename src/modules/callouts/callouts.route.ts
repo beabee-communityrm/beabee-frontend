9743e461ff81a027cbf1bdc2d2bd786fd81d10c7
Will Franklin <will@willpf.co.uk>
2022-02-10 11:40:59 +0000

    chore: add stub callout routes

---
+++ src/modules/callouts/callouts.route.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw } from 'vue-router';
+import i18n from '../../i18n';
+
+const { t } = i18n.global;
+
+export const calloutsRoute: Array<RouteRecordRaw> = [
+  {
+    path: '/callouts',
+    name: 'callouts',
+    component: () => import('./CalloutsPage.vue'),
+    meta: {
+      pageTitle: t('menu.callouts'),
+    },
+  },
+  {
+    path: '/callouts/:id',
+    name: 'callout',
+    component: () => import('./pages/CalloutPage.vue'),
+    meta: {
+      pageTitle: t('menu.callouts'),
+    },
+  },
+];
