b82551791cb6604f0ca0504d5d796e6ae79dbe5c
Basile Simon <basile@basilesimon.fr>
2022-02-22 14:27:28 +0100

    feat: stubs for additional sections

--- src/modules/contacts/pages/ContactOverviewPage.vue
+++ src/modules/contacts/pages/ContactOverviewPage.vue
@@ -17,6 +17,32 @@
     </div>
     <div>
       <AppHeading>{{ t('contactOverview.information') }}</AppHeading>
+      <AppInfoList>
+        <AppInfoListItem :name="'Preferred channel'" :value="'-'" />
+        <AppInfoListItem :name="t('contacts.data.email')" :value="'-'" />
+        <AppInfoListItem :name="t('contacts.data.phone')" :value="'-'" />
+        <AppInfoListItem
+          :name="t('contacts.data.deliveryOptIn')"
+          :value="'-'"
+        />
+        <AppInfoListItem :name="t('contacts.data.newsletter')" :value="'-'" />
+        <AppInfoListItem :name="t('contacts.data.groups')" :value="'-'" />
+      </AppInfoList>
+    </div>
+  </div>
+
+  <div v-if="contact" class="grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
+    <div>
+      <AppHeading>{{ t('contactOverview.contribution') }}</AppHeading>
+      <AppInfoList>
+        <AppInfoListItem :name="t('contacts.data.amount')" :value="'xxx'" />
+        <AppInfoListItem :name="t('contacts.data.period')" :value="'xxx'" />
+        <AppInfoListItem :name="t('contacts.data.payingFee')" :value="'xxx'" />
+        <AppInfoListItem
+          :name="t('contacts.data.contributionType')"
+          :value="'xxx'"
+        />
+      </AppInfoList>
     </div>
   </div>
 </template>
