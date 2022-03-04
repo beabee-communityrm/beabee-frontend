621d8c77e3713849dbd7cc938acab32e07acc590
Will Franklin <will@willpf.co.uk>
2022-03-04 11:03:34 +0000

    feat: add contact payment API
    commit a521b2aaed057eaf89632895e2f2fb07e14878e8
    Author: Will Franklin <will@willpf.co.uk>
    Date:   Fri Feb 25 09:53:39 2022 +0000
    
        chore: add id param
    
    commit 9a0e9916bbfd8f1f77ca33c42328e814e73d0525
    Author: Will Franklin <will@willpf.co.uk>
    Date:   Thu Feb 24 12:03:45 2022 +0000
    
        feat: add fetchPayments for contact payment API

--- src/utils/api/member.ts
+++ src/utils/api/member.ts
@@ -7,6 +7,8 @@ import {
   GetMemberDataWith,
   GetMembersQuery,
   GetMemberWith,
+  GetPaymentData,
+  GetPaymentsQuery,
   Paginated,
   Serial,
   SetContributionData,
@@ -157,4 +159,19 @@ export async function completeUpdatePaymentSource(
     { redirectFlowId }
   );
   return toContrib(data);
+}
+
+export async function fetchPayments(
+  id: string,
+  query: GetPaymentsQuery
+): Promise<Paginated<GetPaymentData>> {
+  const { data } = await axios.get(`/member/${id}/payment`, { params: query });
+  return {
+    ...data,
+    items: data.items.map((item: any) => ({
+      chargeDate: toDate(item.chargeDate),
+      amount: item.amount,
+      status: item.status,
+    })),
+  };
 }
