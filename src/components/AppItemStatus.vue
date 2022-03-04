907dcad20f8697811d5a351731125988f5f70bed
Will Franklin <will@willpf.co.uk>
2022-03-04 16:16:30 +0000

    feat: add status indicator

---
+++ src/components/AppItemStatus.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="text-sm font-bold uppercase" :class="color[status]">{{
+    t('common.status.' + status)
+  }}</span>
+</template>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+const color = {
+  open: 'text-success',
+  finished: 'text-body',
+};
+defineProps<{
+  status: 'open' | 'finished';
+}>();
+</script>
