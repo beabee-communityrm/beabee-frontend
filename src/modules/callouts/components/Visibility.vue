fc47ba0a4db526ab25d1a178655f68ece17efd34
Basile Simon <basile@basilesimon.fr>
2022-03-23 14:51:59 +0100

    chore: match pattern

--- src/modules/callouts/components/Visibility.vue
+++ src/modules/callouts/components/Visibility.vue
@@ -2,7 +2,10 @@
   <AppHeading class="mb-3"> Visibility settings </AppHeading>
   <div class="grid grid-cols-2 gap-6">
     <div class="col-span-1">
-      <AppInput v-model="fooInput" :label="'Something to fill in'"></AppInput>
+      <AppInput
+        v-model="dataProxy.foo"
+        :label="'Something to fill in'"
+      ></AppInput>
     </div>
     <p class="col-span-1 text-sm text-grey mt-6">
       The <b>Title</b> will appear on all the mentions of this callout. It's
@@ -17,17 +20,15 @@ import { ref, computed, watch } from 'vue';
 import AppHeading from '../../../components/AppHeading.vue';
 import AppInput from '../../../components/forms/AppInput.vue';
 
-const props = defineProps<{ data: { foo: string } }>();
-const fooInput = ref(props.data.foo);
-
-const isNotEmptyString = (obj: { value: string }) => obj.value.length > 0;
-const valid = computed((): boolean => isNotEmptyString(fooInput));
-
 const emit = defineEmits(['update:data', 'update:validated']);
-watch(valid, () => {
-  emit('update:data', {
-    foo: fooInput.value,
-  });
-  emit('update:validated', valid.value);
-});
+const props = defineProps<{ data: { foo: string } }>();
+
+const isNotEmptyString = (s: string) => s.length > 0;
+
+const dataProxy = ref(props.data);
+
+watch(
+  () => isNotEmptyString(props.data.foo),
+  (valid) => emit('update:validated', valid)
+);
 </script>
