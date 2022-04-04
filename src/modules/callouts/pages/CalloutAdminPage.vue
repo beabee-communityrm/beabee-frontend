957e9a5ecede914a3356b51d4c262e0cfa2735bc
Will Franklin <will@willpf.co.uk>
2022-04-04 10:07:36 +0100

    Callout admin summary page (#149)
    * chore: add start of callout admin page
    
    * feat: add basic summary page
    
    * feat: add max width for dashboard
    
    * feat: use router-link for internal link
    
    * chore: fix columns layout
    
    * feat: populate some settings
    
    * feat: add some relative dates
    
    * feat: hook on ends with setting
    
    * feat: hook up view button

---
+++ src/modules/callouts/pages/CalloutAdminPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <template v-if="callout">
+    <div class="mb-2">
+      <h1 class="text-2xl">{{ callout.title }}</h1>
+    </div>
+
+    <AppTabs
+      :items="tabs"
+      :selected="route.name ? route.name as string : null"
+    />
+
+    <router-view :callout="callout"></router-view>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
+import AppTabs from '../../../components/tabs/AppTabs.vue';
+import { GetMoreCalloutData } from '../../../utils/api/api.interface';
+import { fetchCallout } from '../../../utils/api/callout';
+
+const route = useRoute();
+
+const { t } = useI18n();
+
+const tabs = computed(() =>
+  callout.value
+    ? [
+        {
+          id: 'calloutAdminOverview',
+          label: t('calloutAdmin.overview'),
+          to: `/admin/callouts/${callout.value.slug}`,
+        },
+        {
+          id: 'calloutAdminResponses',
+          label: t('calloutAdmin.responses'),
+          to: `/admin/callouts/${callout.value.slug}/responses`,
+        },
+      ]
+    : []
+);
+
+const callout = ref<GetMoreCalloutData>();
+
+onBeforeMount(async () => {
+  callout.value = await fetchCallout(route.params.id as string);
+});
+</script>
