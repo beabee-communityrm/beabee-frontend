964c241627fe3e3e34d24e1e2a2d314907c47c33
Basile Simon <basile@basilesimon.fr>
2022-04-06 11:23:37 +0200

    feat: summary details stubs

--- src/components/CalloutSummary.vue
+++ src/components/CalloutSummary.vue
@@ -15,11 +15,28 @@
         <span class="text-link ml-2">{{ baseUrl }}{{ calloutLink }}</span>
       </router-link>
     </p>
+
+    <div class="flex flex-row flex-1 justify-between mt-3">
+      <div class="flex-col">
+        <p><b>36</b> responses so far</p>
+        <router-link :to="calloutResponsesLink">
+          <p class="text-link text-sm">See all responses</p>
+        </router-link>
+      </div>
+      <div class="flex flex-row flex-nowrap">
+        <div class="flex-col">
+          <p><b>OPEN</b> - ends in <b>5</b> days</p>
+          12 Apr - 22 May 2022
+        </div>
+        <AppButton class="ml-2">Edit</AppButton>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { computed } from 'vue';
 import { GetBasicCalloutData } from '../utils/api/api.interface';
+import AppButton from './forms/AppButton.vue';
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
@@ -27,5 +44,6 @@ const props = defineProps<{
   callout: GetBasicCalloutData;
 }>();
 
-const calloutLink = computed(() => `/callouts/${props.callout.slug}`);
+const calloutLink = computed(() => `/admin/callouts/${props.callout.slug}`);
+const calloutResponsesLink = computed(() => `${calloutLink.value}/responses`);
 </script>
