b2b3528f114ef2ab822125f858a0b53facfcae06
Basile Simon <basile@basilesimon.fr>
2022-04-21 16:12:39 +0200

    chore: polish up a bit

--- src/modules/contacts/components/TagDropdown.vue
+++ src/modules/contacts/components/TagDropdown.vue
@@ -1,40 +1,57 @@
 <template>
 	<label v-if="label" class="block mb-1.5 font-semibold">{{ label }}</label>
 
-	<div class="flex flex-row h-[3rem]">
+	<div class="flex flex-col sm:flex-row">
 		<div
-			v-for="tag in assignedTags"
-			class="
-				text-xs
-				font-semibold
-				inline-block
-				py-1
-				px-2
-				uppercase
-				rounded
-				bg-white
-				last:mr-0
-				mr-1
-				h-[1.5rem]
-			"
+			@click="isTagMenuVisible = !isTagMenuVisible"
+			class="cursor-pointer align-middle w-full sm:w-1/2 bg-white p-2"
+			:class="isTagMenuVisible ? 'h-auto' : ''"
 		>
-			<font-awesome-icon
-				class="inline-block cursor-pointer mr-2"
-				icon="times"
-				@click="handleRemoveTag(tag)"
-			/>
-			{{ tag.label }}
+			<p class="align-middle">
+				<font-awesome-icon
+					class="inline-block cursor-pointer mr-2"
+					icon="caret-down"
+					@click="handleRemoveTag(tag)"
+				/>
+				Pick a tag from the list below:
+			</p>
+			<div
+				:class="isTagMenuVisible ? 'block' : 'hidden'"
+				class="divide-y divide-grey-100"
+			>
+				<div
+					v-for="tag in availableTags"
+					class="w-auto p-1 hover:bg-primary-20"
+				>
+					<p @click="handleAddTag(tag)" class="">🏷 {{ tag.label }}</p>
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<div
-		@click="isTagMenuVisible = !isTagMenuVisible"
-		class="cursor-pointer w-full bg-white"
-		:class="isTagMenuVisible ? 'h-[100px]' : 'h-[30px]'"
-	>
-		<div :class="isTagMenuVisible ? 'block' : 'hidden'">
-			<div v-for="tag in availableTags">
-				<p @click="handleAddTag(tag)" class="">🏷 {{ tag.label }}</p>
+		<div class="flex flex-row flex-wrap ml-0 sm:ml-4 mt-2 sm:mt-0">
+			<div
+				v-for="tag in assignedTags"
+				class="
+					text-xs
+					font-semibold
+					inline-block
+					py-1
+					px-2
+					uppercase
+					rounded
+					bg-white
+					last:mr-0
+					mr-1
+					h-[1.5rem]
+					mb-2
+				"
+			>
+				<font-awesome-icon
+					class="inline-block cursor-pointer mr-2"
+					icon="times"
+					@click="handleRemoveTag(tag)"
+				/>
+				{{ tag.label }}
 			</div>
 		</div>
 	</div>
