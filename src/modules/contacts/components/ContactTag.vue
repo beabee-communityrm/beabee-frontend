5629834323d5f99d193f94f7ba79ceada7438d10
Will Franklin <will@willpf.co.uk>
2022-05-05 15:29:29 +0100

    Feature/tag contacts (#176)
    * feat: tagging contacts from expanding box, a la notion
    
    * feat: add/remove tags
    
    * chore: polish up a bit
    
    * feat: tags are part of data model
    
    * chore: style
    
    * feat: component updates model
    
    * feat: load already assigned tags from the API
    
    * chore: style tags
    
    * chore: tags go on top of dropdown
    
    * feat: abstract tag into small component
    
    * feat: use live tags list from API
    
    Co-authored-by: Basile Simon <basile@basilesimon.fr>

---
+++ src/modules/contacts/components/ContactTag.vue
@@ -0,0 +1,32 @@
+<template>
+  <span
+    class="
+      bg-white
+      whitespace-nowrap
+      text-link
+      rounded
+      font-bold
+      inline-block
+      p-1
+      mr-1
+      mb-1
+      text-xs
+      border border-white
+    "
+    :class="hasRemoveIcon ? 'cursor-pointer hover:border-primary-40' : ''"
+  >
+    {{ props.tag }}
+    <font-awesome-icon
+      v-if="props.hasRemoveIcon"
+      class="inline-block ml-2"
+      icon="times"
+    />
+  </span>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  tag: string;
+  hasRemoveIcon?: boolean;
+}>();
+</script>
---
+++ src/modules/contacts/components/TagDropdown.vue
@@ -0,0 +1,81 @@
+<template>
+  <label v-if="label" class="block mb-1.5 font-semibold">{{ label }}</label>
+  <div class="flex flex-row flex-wrap ml-0 mt-2 sm:mt-0">
+    <ContactTag
+      v-for="tag in modelValue"
+      :key="tag"
+      :tag="tag"
+      :has-remove-icon="true"
+      @click="handleRemoveTag(tag)"
+    />
+  </div>
+  <div class="flex flex-col sm:flex-row">
+    <div
+      class="
+        cursor-pointer
+        hover:bg-white
+        border border-primary-40
+        focus:shadow-input
+        rounded
+        align-middle
+        w-full
+        sm:w-1/2
+        bg-white
+        p-2
+      "
+      :class="isTagMenuVisible ? 'h-auto focus:shadow-input' : ''"
+      @click="isTagMenuVisible = !isTagMenuVisible"
+    >
+      <p class="align-middle">
+        <font-awesome-icon
+          class="inline-block cursor-pointer mr-2 ml-2"
+          icon="caret-down"
+        />
+        Pick a tag from the list below
+      </p>
+      <div :class="isTagMenuVisible ? 'block' : 'hidden'" class="">
+        <p
+          v-for="tag in availableTags"
+          :key="tag"
+          class="hover:bg-primary-20 first:mt-2 p-2"
+          @click="handleAddTag(tag)"
+        >
+          <font-awesome-icon
+            class="inline-block cursor-pointer mr-2 ml-2"
+            icon="tag"
+          />
+          {{ tag }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { computed } from '@vue/reactivity';
+import ContactTag from './ContactTag.vue';
+
+const props = defineProps<{
+  tags: string[];
+  modelValue: string[];
+  label?: string;
+}>();
+const emit = defineEmits(['update:modelValue']);
+
+const isTagMenuVisible = ref(false);
+
+const availableTags = computed(() =>
+  props.tags.filter((e) => !props.modelValue.includes(e))
+);
+
+const handleAddTag = (tag: string) => {
+  emit('update:modelValue', [...props.modelValue, tag]);
+};
+const handleRemoveTag = (tag: string) => {
+  emit(
+    'update:modelValue',
+    props.modelValue.filter((e) => e != tag)
+  );
+};
+</script>
