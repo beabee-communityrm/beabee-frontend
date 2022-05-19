4a27b0eb6ce89b726b8af372fa1615e647b72c95
Will Franklin <will@willpf.co.uk>
2022-05-19 16:40:05 +0100

    chore: add title

--- src/components/contribution/ContributionMethod.vue
+++ src/components/contribution/ContributionMethod.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="flex flex-wrap gap-2">
-    <div v-for="method in methods" :key="method" class="flex-1">
-      <button
-        class="
-          h-12
-          w-full
-          rounded
-          border border-primary-40
-          font-semibold
-          cursor-pointer
-        "
-        :class="
-          method === modelValue ? 'bg-link text-white' : 'hover:bg-link-10'
-        "
-        type="button"
-        @click="emit('update:modelValue', method)"
-      >
-        {{ availableMethods[method].label }}
-      </button>
+  <div>
+    <h3 class="font-semibold text-lg mb-1">{{ t('join.paymentMethod') }}</h3>
+    <div class="flex flex-wrap gap-2">
+      <div v-for="method in methods" :key="method" class="flex-1">
+        <button
+          class="
+            h-12
+            w-full
+            rounded
+            border border-primary-40
+            font-semibold
+            cursor-pointer
+          "
+          :class="
+            method === modelValue ? 'bg-link text-white' : 'hover:bg-link-10'
+          "
+          type="button"
+          @click="emit('update:modelValue', method)"
+        >
+          {{ availableMethods[method].label }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
 import { PaymentMethod } from '../../utils/enums/payment-method.enum';
 
 interface MethodOpts {
@@ -39,6 +43,8 @@ const availableMethods: Record<PaymentMethod, MethodOpts> = {
     image: '',
   },
 } as const;
+
+const { t } = useI18n();
 
 const emit = defineEmits(['update:modelValue']);
 defineProps<{
