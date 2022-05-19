f1b42363fce12c059f235bf9772ba35beee7cab3
Will Franklin <will@willpf.co.uk>
2022-05-19 16:09:27 +0100

    feat: support info variant of AppAlert

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="flex items-center px-4 text-white font-semibold rounded"
-    :class="bgColorClass"
-  >
+  <div class="flex items-center px-4 font-semibold rounded" :class="alertClass">
     <div v-if="slots.icon" class="mr-4 text-lg">
       <slot name="icon" />
     </div>
@@ -17,14 +14,16 @@ import { useSlots } from '@vue/runtime-core';
 
 const props = withDefaults(
   defineProps<{
-    variant: 'success' | 'warning' | 'danger';
+    variant: 'success' | 'warning' | 'danger' | 'info';
   }>(),
   { variant: 'success' }
 );
 
 const slots = useSlots();
 
-const bgColorClass = computed(() => {
-  return `bg-${props.variant}`;
-});
+const alertClass = computed(() =>
+  props.variant === 'info'
+    ? 'bg-primary-5 text-primary'
+    : `bg-${props.variant} text-white`
+);
 </script>
