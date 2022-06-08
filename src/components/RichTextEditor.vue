2a4dd2a13fdfd557746c32d7fe0fe860c0317acb
Will Franklin <will@willpf.co.uk>
2022-06-08 11:58:31 +0100

    fix: run prettier on RichTextEditor

--- src/components/RichTextEditor.vue
+++ src/components/RichTextEditor.vue
@@ -1,81 +1,81 @@
 <template>
-	<label v-if="label" class="block mb-1.5 font-semibold">{{ label }} </label>
+  <label v-if="label" class="block mb-1.5 font-semibold">{{ label }} </label>
 
-	<div v-if="editor" class="mb-2 min-h-[2rem] h-auto">
-		<div class="flex flex-row">
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="bold"
-				:class="{ 'is-active': editor.isActive('bold') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleBold().run()"
-				:noMargin="true"
-			/>
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="italic"
-				:class="{ 'is-active': editor.isActive('italic') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleItalic().run()"
-				:noMargin="true"
-			/>
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="underline"
-				:class="{ 'is-active': editor.isActive('underline') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleUnderline().run()"
-				:noMargin="true"
-			/>
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="strikethrough"
-				:class="{ 'is-active': editor.isActive('strike') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleStrike().run()"
-				:noMargin="true"
-			/>
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="heading"
-				:class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-				:noMargin="true"
-			/>
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="list"
-				:class="{ 'is-active': editor.isActive('bulletList') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleBulletList().run()"
-				:noMargin="true"
-			/>
+  <div v-if="editor" class="mb-2 min-h-[2rem] h-auto">
+    <div class="flex flex-row">
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="bold"
+        :class="{ 'is-active': editor.isActive('bold') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleBold().run()"
+        :noMargin="true"
+      />
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="italic"
+        :class="{ 'is-active': editor.isActive('italic') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleItalic().run()"
+        :noMargin="true"
+      />
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="underline"
+        :class="{ 'is-active': editor.isActive('underline') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleUnderline().run()"
+        :noMargin="true"
+      />
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="strikethrough"
+        :class="{ 'is-active': editor.isActive('strike') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleStrike().run()"
+        :noMargin="true"
+      />
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="heading"
+        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+        :noMargin="true"
+      />
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="list"
+        :class="{ 'is-active': editor.isActive('bulletList') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleBulletList().run()"
+        :noMargin="true"
+      />
 
-			<AppButton
-				type="button"
-				variant="primaryOutlined"
-				icon="list-ol"
-				:class="{ 'is-active': editor.isActive('orderedList') }"
-				class="mr-1 mt-1"
-				size="sm"
-				@click="editor.chain().focus().toggleOrderedList().run()"
-				:noMargin="true"
-			/>
+      <AppButton
+        type="button"
+        variant="primaryOutlined"
+        icon="list-ol"
+        :class="{ 'is-active': editor.isActive('orderedList') }"
+        class="mr-1 mt-1"
+        size="sm"
+        @click="editor.chain().focus().toggleOrderedList().run()"
+        :noMargin="true"
+      />
 
-			<!-- <AppButton
+      <!-- <AppButton
 				type="button"
 				variant="primaryOutlined"
 				icon="link"
@@ -85,17 +85,17 @@
 				@click="setLink"
 				>Link</AppButton
 			> -->
-		</div>
-	</div>
-	<editor-content :editor="editor" />
+    </div>
+  </div>
+  <editor-content :editor="editor" />
 </template>
 
 <script lang="ts" setup="{ emit }">
 import {
-	useEditor,
-	EditorContent,
-	BubbleMenu,
-	FloatingMenu,
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
 } from '@tiptap/vue-3';
 import Link from '@tiptap/extension-link';
 import Underline from '@tiptap/extension-underline';
@@ -103,59 +103,59 @@ import StarterKit from '@tiptap/starter-kit';
 import AppButton from './forms/AppButton.vue';
 
 const props = defineProps<{
-	modelValue: string;
-	label?: string;
-	required?: boolean;
+  modelValue: string;
+  label?: string;
+  required?: boolean;
 }>();
 const emit = defineEmits(['update:modelValue']);
 
 const editor = useEditor({
-	content: props.modelValue.value,
-	extensions: [
-		StarterKit,
-		Underline,
-		Link.configure({
-			openOnClick: false,
-		}),
-	],
-	onUpdate: () => emit('update:modelValue', editor.value.getHTML()),
+  content: props.modelValue.value,
+  extensions: [
+    StarterKit,
+    Underline,
+    Link.configure({
+      openOnClick: false,
+    }),
+  ],
+  onUpdate: () => emit('update:modelValue', editor.value.getHTML()),
 });
 
 const setLink = () => {
-	const previousUrl = editor.getMarkAttributes('link').href;
-	const url = window.prompt('URL', previousUrl);
+  const previousUrl = editor.getMarkAttributes('link').href;
+  const url = window.prompt('URL', previousUrl);
 
-	// cancelled
-	if (url === null) {
-		return;
-	}
+  // cancelled
+  if (url === null) {
+    return;
+  }
 
-	// empty
-	if (url === '') {
-		editor.chain().focus().extendMarkRange('link').unsetLink().run();
-		return;
-	}
+  // empty
+  if (url === '') {
+    editor.chain().focus().extendMarkRange('link').unsetLink().run();
+    return;
+  }
 
-	// update link
-	editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
+  // update link
+  editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
 };
 </script>
 
 <style>
 .ProseMirror {
-	@apply p-2 min-h-[5rem] h-auto bg-white w-full border border-primary-40 rounded focus:outline-none focus:shadow-input;
+  @apply p-2 min-h-[5rem] h-auto bg-white w-full border border-primary-40 rounded focus:outline-none focus:shadow-input;
 }
 ul,
 ol {
-	@apply px-4;
+  @apply px-4;
 }
 ul {
-	@apply list-disc;
+  @apply list-disc;
 }
 ol {
-	@apply list-decimal;
+  @apply list-decimal;
 }
 h1 {
-	@apply font-title text-2.5xl;
+  @apply font-title text-2.5xl;
 }
 </style>
