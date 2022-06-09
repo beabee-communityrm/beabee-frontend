5521e0038b98d6e7d85538ae4ee0664b700ef305
Will Franklin <will@willpf.co.uk>
2022-06-09 18:05:37 +0100

    feat: start per country fee pricing

--- src/utils/calcPaymentFee.ts
+++ src/utils/calcPaymentFee.ts
@@ -8,17 +8,29 @@ interface Feeable {
   paymentMethod: PaymentMethod;
 }
 
-const fees: Record<PaymentMethod, (amount: number) => number> = {
-  [PaymentMethod.StripeCard]: (amount) => 0.25 + 0.014 * amount,
-  [PaymentMethod.StripeSEPA]: () => 0.35,
-  [PaymentMethod.GoCardlessDirectDebit]: (amount) => 0.2 + amount / 100,
-};
+const country = import.meta.env.VITE_STRIPE_COUNTRY;
+
+const fees: Record<
+  typeof country,
+  Record<PaymentMethod, (a: number) => number>
+> = {
+  gb: {
+    [PaymentMethod.StripeCard]: (amount) => 0.2 + 0.014 * amount,
+    [PaymentMethod.StripeSEPA]: () => 0.3,
+    [PaymentMethod.GoCardlessDirectDebit]: (amount) => 0.2 + amount / 100,
+  },
+  eu: {
+    [PaymentMethod.StripeCard]: (amount) => 0.25 + 0.014 * amount,
+    [PaymentMethod.StripeSEPA]: () => 0.35,
+    [PaymentMethod.GoCardlessDirectDebit]: (amount) => 0.2 + amount / 100,
+  },
+} as const;
 
 function calcPaymentFee(feeable: Feeable): ComputedRef<number> {
   return computed(() =>
     feeable.period === ContributionPeriod.Annually
       ? 0
-      : fees[feeable.paymentMethod](feeable.amount)
+      : fees[country][feeable.paymentMethod](feeable.amount)
   );
 }
 
