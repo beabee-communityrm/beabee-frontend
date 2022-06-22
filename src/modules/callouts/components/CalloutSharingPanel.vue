28cb9bf81b9e688377aa13354726f3f51bfab193
Ricardo Lafuente <r@manufacturaindependente.org>
2022-06-22 11:28:44 +0100

    feat: add actual share links

--- src/modules/callouts/components/CalloutSharingPanel.vue
+++ src/modules/callouts/components/CalloutSharingPanel.vue
@@ -18,32 +18,72 @@
 			<AppButton icon="copy" size="sm" @click="copyToClipboard">Copy</AppButton>
 		</div>
 
-		Or quickly share using one of these services:
-		<div class="w-2/3 grid gap-2 grid-cols-3 grid-rows-2 mt-3">
-			<div>
-				<font-awesome-icon :icon="['fab', 'facebook']" class="mr-1" />
-				Facebook
-			</div>
-			<div>
-				<font-awesome-icon :icon="['fab', 'linkedin']" class="mr-1" />
-				LinkedIn
-			</div>
-			<div>
-				<font-awesome-icon :icon="['fab', 'telegram']" class="mr-1" />
-				Telegram
-			</div>
-			<div>
-				<font-awesome-icon :icon="['fab', 'twitter']" class="mr-1" /> Twitter
-			</div>
-			<div>
-				<font-awesome-icon :icon="['fab', 'whatsapp']" class="mr-1" />
-				WhatsApp
-			</div>
-			<div>
-				<font-awesome-icon :icon="['fa', 'envelope']" class="mr-1" /> E-mail
-			</div>
-		</div>
-	</div>
+    Or quickly share using one of these services:
+    <div class="w-2/3 grid gap-2 grid-cols-3 grid-rows-2 mt-3">
+      <div>
+        <a
+          :href="
+            'https://www.facebook.com/sharer/sharer.php?u=' +
+            encodeURIComponent(calloutUrl)
+          "
+          target="_blank" rel="nofollow noopener"
+        >
+          <font-awesome-icon :icon="['fab', 'facebook']" class="mr-1" />
+          Facebook
+        </a>
+      </div>
+      <div>
+        <a
+          :href="
+            'https://www.linkedin.com/shareArticle?mini=true&url=' +
+            encodeURIComponent(calloutUrl)
+          "
+          target="_blank" rel="nofollow noopener"
+        >
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="mr-1" />
+          LinkedIn
+        </a>
+      </div>
+      <div>
+        <a
+          :href="
+            'https://telegram.me/share/url?url=' +
+            encodeURIComponent(calloutUrl)
+          "
+          target="_blank" rel="nofollow noopener"
+        >
+          <font-awesome-icon :icon="['fab', 'telegram']" class="mr-1" />
+          Telegram
+        </a>
+      </div>
+      <div>
+        <a
+          :href="
+            'https://twitter.com/share?url=' + encodeURIComponent(calloutUrl)
+          "
+          target="_blank" rel="nofollow noopener"
+        >
+          <font-awesome-icon :icon="['fab', 'twitter']" class="mr-1" />
+          Twitter
+        </a>
+      </div>
+      <div>
+        <a
+          :href="
+            'https://api.whatsapp.com/send?text=' +
+            encodeURIComponent(calloutUrl)
+          "
+          target="_blank" rel="nofollow noopener"
+        >
+          <font-awesome-icon :icon="['fab', 'whatsapp']" class="mr-1" />
+          WhatsApp
+        </a>
+      </div>
+      <div>
+        <font-awesome-icon :icon="['fa', 'envelope']" class="mr-1" /> E-mail
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
