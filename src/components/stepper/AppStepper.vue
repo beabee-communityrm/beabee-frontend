55a9f31e029c711014724649ec25e507143c3bb5
Will Franklin <will@willpf.co.uk>
2022-06-22 18:05:34 +0100

    chore: make stepper sticky

--- src/components/stepper/AppStepper.vue
+++ src/components/stepper/AppStepper.vue
@@ -1,32 +1,34 @@
 <template>
-  <div
-    v-for="(step, stepIndex) in steps"
-    :key="stepIndex"
-    class="flex flex-col"
-  >
+  <div class="sticky top-4">
     <div
-      :class="
-        stepIndex === modelValue
-          ? 'bg-white'
-          : 'bg-primary-5 hover:bg-primary-10 cursor-pointer'
-      "
-      class="p-4 my-1 rounded"
-      @click="emit('update:modelValue', stepIndex)"
+      v-for="(step, stepIndex) in steps"
+      :key="stepIndex"
+      class="flex flex-col"
     >
-      <h4
-        class="flex items-center font-semibold"
-        :class="{ 'text-link': stepIndex === modelValue }"
+      <div
+        :class="
+          stepIndex === modelValue
+            ? 'bg-white'
+            : 'bg-primary-5 hover:bg-primary-10 cursor-pointer'
+        "
+        class="p-4 my-1 rounded"
+        @click="emit('update:modelValue', stepIndex)"
       >
-        <span
-          class="inline-block w-4 h-4 border-2 rounded-full align-middle mr-2"
-          :class="{
-            'bg-link': step.validated,
-            'border-link': step.validated || stepIndex === modelValue,
-          }"
-        />
-        {{ step.name }}
-      </h4>
-      <p class="text-sm text-grey mt-1">{{ step.description }}</p>
+        <h4
+          class="flex items-center font-semibold"
+          :class="{ 'text-link': stepIndex === modelValue }"
+        >
+          <span
+            class="inline-block w-4 h-4 border-2 rounded-full align-middle mr-2"
+            :class="{
+              'bg-link': step.validated,
+              'border-link': step.validated || stepIndex === modelValue,
+            }"
+          />
+          {{ step.name }}
+        </h4>
+        <p class="text-sm text-grey mt-1">{{ step.description }}</p>
+      </div>
     </div>
   </div>
 </template>
