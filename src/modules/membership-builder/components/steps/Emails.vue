8d4076cd11eb045aa6b8295e7dc4edaa835b58fa
Will Franklin <will@willpf.co.uk>
2022-06-23 12:28:29 +0100

    chore: add more fields to email step

--- src/modules/membership-builder/components/steps/Emails.vue
+++ src/modules/membership-builder/components/steps/Emails.vue
@@ -1,12 +1,29 @@
 <template>
   <form @submit.prevent>
     <AppHeading class="mb-5">Welcome & goodbye emails</AppHeading>
-    <AppSubHeading>Welcome email</AppSubHeading>
-    <AppSubHeading>Cancellation email</AppSubHeading>
+    <div class="flex mb-8">
+      <div class="flex-1">
+        <AppSubHeading class="mb-4">Welcome email</AppSubHeading>
+        <AppInput label="Subject" class="mb-4" />
+        <RichTextEditor label="Message" />
+      </div>
+      <div class="flex-1"></div>
+    </div>
+
+    <div class="flex">
+      <div class="flex-1">
+        <AppSubHeading class="mb-4">Cancellation email</AppSubHeading>
+        <AppInput label="Subject" class="mb-4" />
+        <RichTextEditor label="Message" />
+      </div>
+      <div class="flex-1"></div>
+    </div>
   </form>
 </template>
 
 <script lang="ts" setup>
 import AppHeading from '../../../../components/AppHeading.vue';
 import AppSubHeading from '../../../../components/AppSubHeading.vue';
+import AppInput from '../../../../components/forms/AppInput.vue';
+import RichTextEditor from '../../../../components/rte/RichTextEditor.vue';
 </script>
