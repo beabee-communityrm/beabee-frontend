bcd40042df494299e733479b375adb7e4268c69c
Will Franklin <will@willpf.co.uk>
2022-06-27 12:40:03 +0100

    chore: add BACS icon

--- src/components/payment-method/PaymentMethodIcon.vue
+++ src/components/payment-method/PaymentMethodIcon.vue
@@ -4,8 +4,8 @@
 
 <script lang="ts" setup>
 import { PaymentMethod } from '../../utils/enums/payment-method.enum';
+import BACS from './icons/BACS.vue';
 import Card from './icons/Card.vue';
-import DirectDebit from './icons/DirectDebit.vue';
 import SEPA from './icons/SEPA.vue';
 
 defineProps<{ method: PaymentMethod }>();
@@ -13,7 +13,7 @@ defineProps<{ method: PaymentMethod }>();
 const icons = {
   [PaymentMethod.StripeCard]: Card,
   [PaymentMethod.StripeSEPA]: SEPA,
-  [PaymentMethod.StripeBACS]: DirectDebit,
-  [PaymentMethod.GoCardlessDirectDebit]: DirectDebit,
+  [PaymentMethod.StripeBACS]: BACS,
+  [PaymentMethod.GoCardlessDirectDebit]: BACS,
 };
 </script>
