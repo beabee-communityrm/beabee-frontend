235eaa9a8089ac55129fe74a52f2a92a13fb4b58
Will Franklin <will@willpf.co.uk>
2022-08-17 18:03:23 +0100

    feat: move theme logic to separate file, watch for changes

--- src/store/generalContent.ts
+++ src/store/generalContent.ts
@@ -1,31 +1,6 @@
 import { ref } from 'vue';
-import { mix, parseToRgba, transparentize } from 'color2k';
 import { GeneralContent } from '../utils/api/api.interface';
 import { fetchContent } from '../utils/api/content';
-
-function setCSSVar(name: string, value: string) {
-  document.documentElement.style.setProperty(name, value);
-}
-
-function setColorVar(name: string, color: string) {
-  const [r, g, b] = parseToRgba(color);
-  setCSSVar(name, `${r}, ${g}, ${b}`);
-}
-
-function setShades(
-  colorName: string,
-  colorValue: string,
-  levels: number[] = []
-) {
-  setColorVar(`--c-${colorName}`, colorValue);
-  for (const level of levels) {
-    const levelColor =
-      level > 100
-        ? mix(colorValue, 'black', level / 100 - 1)
-        : mix(colorValue, 'white', 1 - level / 100);
-    setColorVar(`--c-${colorName}-${level}`, levelColor);
-  }
-}
 
 export const generalContent = ref<GeneralContent>({
   organisationName: '',
@@ -42,26 +17,5 @@ export const generalContent = ref<GeneralContent>({
 
 export const initGeneralContent = fetchContent('general').then((content) => {
   generalContent.value = content;
-
-  // Load the theme
-  const theme = generalContent.value.theme;
-  const fonts = theme.fonts || {};
-
-  const primaryColor = theme.colors?.primary || '#262453';
-  const linkColor = theme.colors?.link || primaryColor;
-  const bodyFont = fonts.body || '"Open Sans", sans-serif';
-
-  setShades('primary', primaryColor, [5, 10, 20, 40, 70, 80]);
-  setShades('body', theme.colors?.body || primaryColor, [40, 80]);
-  setShades('link', linkColor, [10, 70, 110]);
-  setShades('warning', theme.colors?.warning || '#f5cc5b', [10, 30, 70]);
-  setShades('success', theme.colors?.success || '#86a960', [10, 30, 70, 110]);
-  setShades('danger', theme.colors?.danger || '#ce3d3d', [10, 30, 70, 110]);
-  setShades('white', theme.colors?.white || '#ffffff');
-  setShades('black', theme.colors?.black || '#000000');
-
-  setCSSVar('--ff-body', bodyFont);
-  setCSSVar('--ff-title', fonts.title || bodyFont);
-
-  setCSSVar('--bs-input', '0 0 0 0.125em ' + transparentize(linkColor, 0.75));
+  return content;
 });
---
+++ src/theme.ts
@@ -0,0 +1,92 @@
+import { parseToRgba, mix, transparentize } from 'color2k';
+import { watch } from 'vue';
+import { generalContent } from './store';
+
+export type Theme = {
+  colors: {
+    primary: string;
+    link: string;
+    body: string;
+    success: string;
+    warning: string;
+    danger: string;
+    white: string;
+    black: string;
+  };
+  fonts: {
+    title: string;
+    body: string;
+  };
+};
+
+export type PartialTheme = { [T in keyof Theme]?: Partial<Theme[T]> };
+
+function setCSSVar(name: string, value: string) {
+  document.documentElement.style.setProperty(name, value);
+}
+
+function setColorVar(name: string, color: string) {
+  const [r, g, b] = parseToRgba(color);
+  setCSSVar(name, `${r}, ${g}, ${b}`);
+}
+
+function setShades(
+  colorName: string,
+  colorValue: string,
+  levels: number[] = []
+) {
+  setColorVar(`--c-${colorName}`, colorValue);
+  for (const level of levels) {
+    const levelColor =
+      level > 100
+        ? mix(colorValue, 'black', level / 100 - 1)
+        : mix(colorValue, 'white', 1 - level / 100);
+    setColorVar(`--c-${colorName}-${level}`, levelColor);
+  }
+}
+
+export function getFullTheme(theme: PartialTheme): Theme {
+  const primaryColor = theme.colors?.primary || '#262453';
+  const bodyFont = theme.fonts?.body || '"Open Sans", sans-serif';
+
+  return {
+    colors: {
+      primary: primaryColor,
+      body: theme.colors?.body || primaryColor,
+      link: theme.colors?.link || primaryColor,
+      warning: theme.colors?.warning || '#f5cc5b',
+      success: theme.colors?.success || '#86a960',
+      danger: theme.colors?.danger || '#ce3d3d',
+      white: theme.colors?.white || '#ffffff',
+      black: theme.colors?.black || '#000000',
+    },
+    fonts: {
+      body: bodyFont,
+      title: theme.fonts?.title || bodyFont,
+    },
+  };
+}
+
+watch(
+  () => generalContent.value.theme,
+  (newTheme) => {
+    const { colors, fonts } = getFullTheme(newTheme);
+
+    setShades('primary', colors.primary, [5, 10, 20, 40, 70, 80]);
+    setShades('body', colors.body, [40, 80]);
+    setShades('link', colors.link, [10, 70, 110]);
+    setShades('warning', colors.warning || '#f5cc5b', [10, 30, 70]);
+    setShades('success', colors.success || '#86a960', [10, 30, 70, 110]);
+    setShades('danger', colors.danger || '#ce3d3d', [10, 30, 70, 110]);
+    setShades('white', colors.white || '#ffffff');
+    setShades('black', colors.black || '#000000');
+
+    setCSSVar('--ff-body', fonts.body);
+    setCSSVar('--ff-title', fonts.title);
+
+    setCSSVar(
+      '--bs-input',
+      '0 0 0 0.125em ' + transparentize(colors.link, 0.75)
+    );
+  }
+);
