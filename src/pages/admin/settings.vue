795505fb494cb4b07ac91d54f9283c28033a63be
Will Franklin <will@willpf.co.uk>
2022-08-24 16:51:31 +0100

    fix: tabs not updating when locale changes

--- src/pages/admin/settings.vue
+++ src/pages/admin/settings.vue
@@ -14,28 +14,30 @@ meta:
 import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
 import PageTitle from '../../components/PageTitle.vue';
-import { TabItem } from '../../components/tabs/tabs.interface';
 import AppTabs from '../../components/tabs/AppTabs.vue';
+import { computed } from 'vue';
 
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
 
-const tabs: TabItem[] = [
-  {
-    id: 'adminSettingsGeneral',
-    label: t('adminSettings.general.label'),
-  },
-  {
-    id: 'adminSettingsEmail',
-    label: t('adminSettings.email.label'),
-  },
-  {
-    id: 'adminSettingsTheme',
-    label: t('adminSettings.theme.label'),
-  },
-].map((item) => ({
-  ...item,
-  to: router.resolve({ name: item.id }).href,
-}));
+const tabs = computed(() =>
+  [
+    {
+      id: 'adminSettingsGeneral',
+      label: t('adminSettings.general.label'),
+    },
+    {
+      id: 'adminSettingsEmail',
+      label: t('adminSettings.email.label'),
+    },
+    {
+      id: 'adminSettingsTheme',
+      label: t('adminSettings.theme.label'),
+    },
+  ].map((item) => ({
+    ...item,
+    to: router.resolve({ name: item.id }).href,
+  }))
+);
 </script>
