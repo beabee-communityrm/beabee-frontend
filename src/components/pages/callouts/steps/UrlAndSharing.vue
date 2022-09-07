4dead85ceaf91ae4ba62e720e032e0e963a884e9
Will Franklin <will@willpf.co.uk>
2022-09-07 12:19:32 +0100

    fix: inconsistent margin in url and sharing step

--- src/components/pages/callouts/steps/UrlAndSharing.vue
+++ src/components/pages/callouts/steps/UrlAndSharing.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="grid grid-cols-2 gap-6" v-if="mode === 'new'">
+    <div v-if="mode === 'new'" class="grid grid-cols-2 gap-6 mt-5">
       <div class="col-span-1">
         <AppRadioGroup
           v-model="dataProxy.useCustomSlug"
@@ -28,7 +28,7 @@
         v-html="inputT('slug.help')"
       />
     </div>
-    <div class="grid grid-cols-2 gap-6 mt-6">
+    <div class="grid grid-cols-2 gap-6 mt-5">
       <div class="col-span-1">
         <AppRadioGroup
           v-model="dataProxy.overrideShare"
@@ -64,7 +64,7 @@
           :label="inputT('description.label')"
           :placeholder="inputT('description.placeholder')"
           required
-        ></AppTextArea>
+        />
       </div>
       <div
         class="col-span-1 text-sm text-grey mt-6"
