827ffc746a39cc9adcf854ff855cd6e2e8a65eee
Will Franklin <will@willpf.co.uk>
2022-10-10 18:17:19 +0100

    fix: size changing between relative and text inputs

--- src/components/search/operators/DateInput.vue
+++ src/components/search/operators/DateInput.vue
@@ -4,30 +4,34 @@
   >
     <button
       type="button"
-      class="bg-primary-20 px-2"
+      class="flex-0 bg-primary-20 px-2"
       @click="isRelative = !isRelative"
     >
       <font-awesome-icon
         :icon="['fa', isRelative ? 'circle-arrow-right' : 'circle-dot']"
       />
     </button>
-    <input
-      v-if="isRelative"
-      v-model="relativeValue"
-      type="text"
-      class="p-2 leading-tight focus:outline-none"
-      placeholder="$now(d:-1)"
-    />
-    <input
-      v-else
-      v-model="dateValue"
-      type="date"
-      class="p-2 leading-tight focus:outline-none"
-    />
+    <div>
+      <input
+        v-if="isRelative"
+        v-model="relativeValue"
+        type="text"
+        class="block w-[10rem] p-2 leading-tight focus:outline-none"
+        placeholder="$now(d:-1)"
+      />
+      <input
+        v-else
+        v-model="dateValue"
+        type="date"
+        class="block w-[10rem] p-2 leading-tight focus:outline-none"
+      />
+    </div>
   </span>
 </template>
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
+import { computed, ref, watch } from 'vue';
 const emit = defineEmits(['update:modelValue']);
 const props = defineProps<{ modelValue: string }>();
 
@@ -36,10 +40,13 @@ const isRelative = ref(props.modelValue.startsWith('$now'));
 const relativeValue = ref(isRelative.value ? props.modelValue : '');
 const dateValue = ref(isRelative.value ? '' : props.modelValue);
 
-watch([isRelative, relativeValue, dateValue], () => {
-  emit(
-    'update:modelValue',
-    isRelative.value ? relativeValue.value : dateValue.value
-  );
+const currentValue = computed(() =>
+  isRelative.value ? relativeValue.value : dateValue.value
+);
+
+watch(currentValue, (newValue) => {
+  emit('update:modelValue', newValue);
 });
+
+useVuelidate({ value: { required } } as any, { value: currentValue } as any);
 </script>
