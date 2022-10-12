a3586854ca4c70e7845ba795ab2fb202d1c6bf4f
Will Franklin <will@willpf.co.uk>
2022-10-12 10:41:38 +0100

    chore: get currency symbol from API

--- src/utils/api/api.interface.ts
+++ src/utils/api/api.interface.ts
@@ -252,6 +252,7 @@ export interface GeneralContent {
   impressumLink?: string;
   locale: string;
   currencyCode: string;
+  currencySymbol: string;
   hideContribution?: boolean;
   footerLinks?: { text: string; url: string }[];
   theme: {
