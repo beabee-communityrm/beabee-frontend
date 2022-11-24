efa01680f141e9697008a66aa787363027d171f5
Ricardo Lafuente <r@manufacturaindependente.org>
2022-11-24 12:03:47 +0000

    feat: use app tabs instead of stepper in memb-builder (WIP)

--- src/pages/admin/membership-builder/index.vue
+++ src/pages/admin/membership-builder/index.vue
@@ -30,80 +30,50 @@ meta:
   </PageTitle>
   <div class="flex gap-8">
     <div class="flex-0 basis-menu">
-      <AppStepper v-model="selectedStepIndex" :steps="steps" />
-    </div>
-    <div class="flex-1">
-      <div
-        v-for="(step, i) in steps"
-        :key="i"
-        :class="{ hidden: selectedStepIndex !== i }"
-      >
-        <component
-          :is="step.component"
-          v-model:error="step.error"
-          v-model:validated="step.validated"
-          :emitter="emitter"
-        />
-      </div>
+      <!-- <AppTabs v-model="selectedStepIndex" :steps="steps" /> -->
+      <AppTabs :items="tabs" :selected="route.name ? route.name as string : null" /> -->
+      
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import mitt from 'mitt';
-import { markRaw, Ref, ref } from 'vue';
-import type { Component } from 'vue';
+import { Ref, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import PageTitle from '../../../components/PageTitle.vue';
-import AppStepper from '../../../components/stepper/AppStepper.vue';
-import { Step } from '../../../components/stepper/stepper.interface';
-import EditJoinForm from '../../../components/pages/admin/membership-builder/steps/EditJoinForm.vue';
-import AccountConfirmation from '../../../components/pages/admin/membership-builder/steps/AccountConfirmation.vue';
-import IntroMessage from '../../../components/pages/admin/membership-builder/steps/IntroMessage.vue';
-import Emails from '../../../components/pages/admin/membership-builder/steps/EmailsStep.vue';
+import AppTabs from '../../../components/tabs/AppTabs.vue';
 import AppButton from '../../../components/forms/AppButton.vue';
 import { MembershipBuilderEmitter } from '../../../components/pages/admin/membership-builder/membership-builder.interface';
 import useVuelidate from '@vuelidate/core';
+import { TabItem } from '../../../components/tabs/tabs.interface';
+import { useRoute } from 'vue-router';
 
 const { t } = useI18n();
+const route = useRoute();
 
-const selectedStepIndex = ref(0);
 const updated = ref(false);
 const updating = ref(false);
 const dirty = ref(false);
 
-interface BuilderStep extends Step {
-  component: Component;
-}
+// TODO: i18n
+const tabs: Ref<TabItem[]> = ref([
+  { id: 'joinForm',
+    label: 'Join form',
+    to: `/admin/membership-builder/join-form`,
+  },
+  { id: 'confirmation',
+    label: 'Account confirmation',
+    to: `/admin/membership-builder/confirmation`,
+  },
+  { id: 'introMessage',
+    label: 'Intro message',
+    to: `/admin/membership-builder/intro`,
+  },
+  { id: 'emails',
+    label: 'Emails',
+    to: `/admin/membership-builder/emails`,
+  },
 
-const steps: Ref<BuilderStep[]> = ref([
-  {
-    name: t('membershipBuilder.steps.joinForm.title'),
-    description: t('membershipBuilder.steps.joinForm.description'),
-    validated: true,
-    error: false,
-    component: markRaw(EditJoinForm),
-  },
-  {
-    name: t('membershipBuilder.steps.accountConfirmation.title'),
-    description: t('membershipBuilder.steps.accountConfirmation.description'),
-    validated: true,
-    error: false,
-    component: markRaw(AccountConfirmation),
-  },
-  {
-    name: t('membershipBuilder.steps.intro.title'),
-    description: t('membershipBuilder.steps.intro.description'),
-    validated: true,
-    error: false,
-    component: markRaw(IntroMessage),
-  },
-  {
-    name: t('membershipBuilder.steps.emails.title'),
-    description: t('membershipBuilder.steps.emails.description'),
-    validated: true,
-    error: false,
-    component: markRaw(Emails),
-  },
 ]);
 
 const validation = useVuelidate();
@@ -116,16 +86,10 @@ emitter.on('dirty', () => {
 });
 
 async function handleUpdate() {
-  let updatedNo = 0;
-
   function handleUpdated() {
-    updatedNo++;
-    if (updatedNo === steps.value.length) {
-      updating.value = false;
-      dirty.value = false;
-      emitter.off('updated', handleUpdated);
-      updated.value = true;
-    }
+    dirty.value = false;
+    emitter.off('updated', handleUpdated);
+    updated.value = true;
   }
   updating.value = true;
 
---
+++ src/pages/admin/membership-builder/join-form.vue
@@ -0,0 +1,18 @@
+<route lang="yaml">
+name: adminMembershipBuilderJoinForm
+meta:
+  pageTitle: membershipBuilder.title
+  role: admin
+</route>
+
+<template>
+  <div class="grid lg:grid-cols-2 xl:grid-cols-3">
+    <Suspense>
+      <EditJoinForm />
+    </Suspense>
+  </div>
+</template>
+<script lang="ts" setup>
+import EditJoinForm from '../../../components/pages/admin/membership-builder/steps/EditJoinForm.vue';
+
+</script>
