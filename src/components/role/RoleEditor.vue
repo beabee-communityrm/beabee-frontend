2ab70f7bff1019c519cb5fc4347a7d16ef83107a
Will Franklin <will@willpf.co.uk>
2022-12-02 14:46:04 +0000

    feat: add role editor to add contact and make improvements

--- src/components/role/RoleEditor.vue
+++ src/components/role/RoleEditor.vue
@@ -8,37 +8,54 @@
       @update="emit('update', $event)"
     />
 
-    <AppButton
-      class="mt-3 w-full"
-      variant="primaryOutlined"
-      @click="formVisible = !formVisible"
-    >
-      {{ t('roleEditor.add') }}
-    </AppButton>
-
-    <RoleEditorForm
-      v-if="formVisible"
-      class="rounded rounded-t-none border border-primary-20 bg-primary-10 p-4"
-      @cancel="formVisible = false"
-      @save="handleSaveNew"
-    />
+    <template v-if="availableRoles.length > 0">
+      <div
+        v-if="formVisible"
+        class="mt-3 rounded rounded-t-none border border-primary-20 bg-primary-10 p-4"
+      >
+        <AppSubHeading class="mb-4">
+          {{ t('roleEditor.addNewRole') }}
+        </AppSubHeading>
+        <RoleEditorForm
+          :available-roles="availableRoles"
+          @cancel="formVisible = false"
+          @save="handleSaveNew"
+        />
+      </div>
+      <AppButton
+        v-else
+        class="mt-3 w-full"
+        variant="primaryOutlined"
+        @click="formVisible = true"
+      >
+        {{ t('roleEditor.add') }}
+      </AppButton>
+    </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import AppButton from '../forms/AppButton.vue';
 import RoleEditorItem from './RoleEditorItem.vue';
 import { MemberRoleData } from '../../utils/api/api.interface';
 import RoleEditorForm from './RoleEditorForm.vue';
+import { PermissionTypes } from '@beabee/beabee-common';
+import AppSubHeading from '../AppSubHeading.vue';
 
 const emit = defineEmits(['delete', 'update']);
-defineProps<{ roles: MemberRoleData[] }>();
+const props = defineProps<{ roles: MemberRoleData[] }>();
 
 const { t } = useI18n();
 
 const formVisible = ref(false);
+
+const availableRoles = computed(() =>
+  PermissionTypes.filter((type) =>
+    props.roles.every((role) => role.role !== type)
+  )
+);
 
 async function handleSaveNew(role: MemberRoleData) {
   formVisible.value = false;
