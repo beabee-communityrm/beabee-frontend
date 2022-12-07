b79b1e9aa4b6a5b7c4f7ae2c0f2cc5add02f4e8b
Ricardo Lafuente <rlafuente@users.noreply.github.com>
2022-12-07 11:29:09 +0000

    Contact contribution edit form (#306)
    * feat: first stab at a contribution edit form
    
    * feat: contact contribution edit in admin (wip)
    
    * fix: add remaining reference widget
    
    * chore: contribution edit form with proper structure
    
    * chore: set up contribution form (wip)
    
    * feat: working contribution edit form
    
    * fix: cleanup and last fixes for a working form
    
    * fix: address some of the tsc warnings
    
    * fix: move component to its rightful place
    
    * chore: i18n and fetch payment sources
    
    * chore: update locales
    
    * feat: show or hide contrib edit according to type
    
    * fix: address some tsc warnings
    
    * fix: support undefined on radio group and select
    
    * fix: improve type of ForceUpdateContributionData
    
    * fix: can't update to automatic
    
    * chore: hacky fix for contribution type
    
    * chore: i18n
    
    * fix: better contrib type detection
    
    * fix: add missing translation import
    
    * fix: reference to i18n string
    
    * fix: can't currently set margins on AppInputs
    
    * chore: add currency symbol as prefix
    
    * refactor: use AppForm component
    
    * refactor: use existing contact to avoid refetching from API
    
    * chore: hide inputs for ContributionType none
    
    * feat: add PaymentHistory as quick win for auto contributions
    
    * fix: add standard gap to grid
    
    * chore: add MessageBox warning for notice
    
    * chore: allow empty payment source
    
    * refactor: revert to simpler per tab fetch for now
    
    * chore: hide form while loading data
    
    * chore: update i18n
    
    Co-authored-by: Will Franklin <will@willpf.co.uk>

---
+++ src/pages/admin/contacts/[id]/contribution.vue
@@ -0,0 +1,33 @@
+<route lang="yaml">
+name: adminContactsViewContribution
+meta:
+  pageTitle: menu.community
+  role: admin
+</route>
+
+<template>
+  <div class="grid gap-8 lg:grid-cols-2 xl:grid-cols-3">
+    <div>
+      <AppHeading class="mb-2">
+        {{ t('contribution.billing') }}
+      </AppHeading>
+      <Suspense>
+        <EditManualContribution :id="contact.id" />
+      </Suspense>
+    </div>
+    <div>
+      <PaymentsHistory :id="contact.id" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { GetMemberData } from '../../../../utils/api/api.interface';
+import EditManualContribution from '../../../../components/pages/admin/contacts/EditManualContribution.vue';
+import { useI18n } from 'vue-i18n';
+import PaymentsHistory from '../../../../components/pages/profile/contribution/PaymentsHistory.vue';
+import AppHeading from '../../../../components/AppHeading.vue';
+
+const { t } = useI18n();
+
+defineProps<{ contact: GetMemberData }>();
+</script>
