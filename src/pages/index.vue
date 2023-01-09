60410fe8e4174b62c6b4ed366a3f4c418c118b8a
Will Franklin <will@willpf.co.uk>
2023-01-09 11:34:32 +0000

    chore: add redirect from root / (#338)

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<route lang="yaml">
+meta:
+  pageTitle: menu.home
+</route>
+<template><div /></template>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { canAdmin } from '../utils/currentUserCan';
+useRouter().replace(canAdmin.value ? '/admin' : '/profile');
+</script>
