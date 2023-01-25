8f73a9941567ff11f1320923d6b6056ce7d8a29b
Will Franklin <will@willpf.co.uk>
2023-01-25 18:35:38 +0000

    Move membership builder to 2 col grid

--- src/components/App2ColGrid.vue
+++ src/components/App2ColGrid.vue
@@ -5,10 +5,13 @@
     </div>
     <div
       v-if="$slots.col2"
-      class="col-span-12 md:col-span-6 xl:col-start-6 xl:col-end-10"
+      class="col-span-12 md:col-span-6 xl:col-start-6"
+      :class="extended ? 'xl:col-end-13' : 'xl:col-end-10'"
     >
       <slot name="col2" />
     </div>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+defineProps<{ extended?: boolean }>();
+</script>
--- src/pages/admin/membership-builder/confirmation.vue
+++ src/pages/admin/membership-builder/confirmation.vue
@@ -6,92 +6,96 @@ meta:
 </route>
 
 <template>
-  <div class="mb-8 grid grid-cols-2 gap-8">
-    <div>
+  <App2ColGrid class="mb-8">
+    <template #col1>
       <p>{{ stepT('text') }}</p>
-    </div>
-  </div>
-  <div v-if="setupContent" class="grid grid-cols-2 gap-8">
-    <AppForm
-      :button-text="t('form.saveChanges')"
-      :success-text="t('form.saved')"
-      @submit="handleUpdate"
-    >
-      <div class="mb-4">
-        <AppInput
-          v-model="setupContent.welcome"
-          :label="stepT('welcomeMessage')"
+    </template>
+  </App2ColGrid>
+  <App2ColGrid v-if="setupContent" extended>
+    <template #col1>
+      <AppForm
+        :button-text="t('form.saveChanges')"
+        :success-text="t('form.saved')"
+        @submit="handleUpdate"
+      >
+        <div class="mb-4">
+          <AppInput
+            v-model="setupContent.welcome"
+            :label="stepT('welcomeMessage')"
+          />
+        </div>
+
+        <AppCheckbox
+          v-model="setupContent.showMailOptIn"
+          :label="stepT('showMailOptIn')"
+          class="mb-4 font-semibold"
         />
+
+        <template v-if="setupContent.showMailOptIn">
+          <div class="mb-4">
+            <AppInput
+              v-model="setupContent.mailTitle"
+              :label="stepT('optInTitle')"
+              required
+            />
+          </div>
+          <div class="mb-4">
+            <RichTextEditor
+              v-model="setupContent.mailText"
+              :label="stepT('optInText')"
+              required
+            />
+          </div>
+          <div class="mb-4">
+            <AppInput
+              v-model="setupContent.mailOptIn"
+              :label="stepT('optInLabel')"
+              required
+            />
+          </div>
+        </template>
+
+        <AppCheckbox
+          v-model="setupContent.showNewsletterOptIn"
+          :label="stepT('showNewsletterOptIn')"
+          class="mb-4 font-semibold"
+        />
+
+        <template v-if="setupContent.showNewsletterOptIn">
+          <div class="mb-4">
+            <AppInput
+              v-model="setupContent.newsletterTitle"
+              :label="stepT('optInTitle')"
+              required
+            />
+          </div>
+          <div class="mb-4">
+            <RichTextEditor
+              v-model="setupContent.newsletterText"
+              :label="stepT('optInText')"
+              required
+            />
+          </div>
+          <div class="mb-4">
+            <AppInput
+              v-model="setupContent.newsletterOptIn"
+              :label="stepT('optInLabel')"
+              required
+            />
+          </div>
+        </template>
+      </AppForm>
+    </template>
+    <template #col2>
+      <div class="bg-cover bg-center p-4 pt-8" :style="backgroundStyle">
+        <Suspense>
+          <AuthBox>
+            <SetupForm :setup-content="setupContent" />
+          </AuthBox>
+        </Suspense>
       </div>
-
-      <AppCheckbox
-        v-model="setupContent.showMailOptIn"
-        :label="stepT('showMailOptIn')"
-        class="mb-4 font-semibold"
-      />
-
-      <template v-if="setupContent.showMailOptIn">
-        <div class="mb-4">
-          <AppInput
-            v-model="setupContent.mailTitle"
-            :label="stepT('optInTitle')"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <RichTextEditor
-            v-model="setupContent.mailText"
-            :label="stepT('optInText')"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <AppInput
-            v-model="setupContent.mailOptIn"
-            :label="stepT('optInLabel')"
-            required
-          />
-        </div>
-      </template>
-
-      <AppCheckbox
-        v-model="setupContent.showNewsletterOptIn"
-        :label="stepT('showNewsletterOptIn')"
-        class="mb-4 font-semibold"
-      />
-
-      <template v-if="setupContent.showNewsletterOptIn">
-        <div class="mb-4">
-          <AppInput
-            v-model="setupContent.newsletterTitle"
-            :label="stepT('optInTitle')"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <RichTextEditor
-            v-model="setupContent.newsletterText"
-            :label="stepT('optInText')"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <AppInput
-            v-model="setupContent.newsletterOptIn"
-            :label="stepT('optInLabel')"
-            required
-          />
-        </div>
-      </template>
-    </AppForm>
-    <div class="bg-cover bg-center p-4 pt-8">
-      <Suspense>
-        <AuthBox>
-          <SetupForm :setup-content="setupContent" />
-        </AuthBox>
-      </Suspense>
-    </div>
-  </div>
+    </template>
+  </App2ColGrid>
 </template>
 <script lang="ts" setup>
 import { onBeforeMount, ref } from 'vue';
@@ -104,6 +108,8 @@ import AppCheckbox from '../../../components/forms/AppCheckbox.vue';
 import SetupForm from '../../../components/pages/join/SetupForm.vue';
 import AuthBox from '../../../components/AuthBox.vue';
 import RichTextEditor from '../../../components/rte/RichTextEditor.vue';
+import App2ColGrid from '../../../components/App2ColGrid.vue';
+import backgroundStyle from '../../../utils/backgroundStyle';
 
 const setupContent = ref<JoinSetupContent>();
 
