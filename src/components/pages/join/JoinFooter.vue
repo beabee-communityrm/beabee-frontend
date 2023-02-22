540cdb52a29f2c33ed5e5382e9827c87b2e9fec0
Will Franklin <will@willpf.co.uk>
2023-02-22 09:56:37 +0000

    Add powered by beabee logo

--- src/components/pages/join/JoinFooter.vue
+++ src/components/pages/join/JoinFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-center text-xs">
+  <p class="mb-2 text-center text-xs">
     {{ t('join.notice') }}
     <a
       class="text-link underline hover:text-primary"
@@ -9,10 +9,17 @@
       >{{ t('join.privacy') }}</a
     >.
   </p>
+  <p class="text-center text-xs">
+    <a href="https://beabee.io" target="_blank">
+      <img class="inline" :src="beabeeLogo" width="30" />
+      {{ t('join.poweredBy') }}
+    </a>
+  </p>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
+import beabeeLogo from '../../../assets/images/beabee-logo.png';
 
 const { t } = useI18n();
 
