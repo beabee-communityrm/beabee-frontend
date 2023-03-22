143c81ed396cb1f68d606a95762b61b1121a3067
Will Franklin <will@willpf.co.uk>
2023-03-22 15:34:21 +0000

    Move backgroundStyle to generalContent store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { currentUser, initCurrentUser, updateCurrentUser } from './currentUser';
-import { generalContent, initGeneralContent } from './generalContent';
+import {
+  generalContent,
+  initGeneralContent,
+  backgroundStyle,
+} from './generalContent';
 
 export const initStore = Promise.all([initCurrentUser, initGeneralContent]);
 
-export { currentUser, updateCurrentUser, generalContent };
+export { currentUser, updateCurrentUser, generalContent, backgroundStyle };
