c05b1ac3480fa0a759aa3fea3f2b6f501b30e875
Will Franklin <will@willpf.co.uk>
2023-08-02 16:24:38 +0100

    Add basic prooperty view

--- src/pages/callouts/[id]/map.vue
+++ src/pages/callouts/[id]/map.vue
@@ -54,11 +54,81 @@ meta:
             }"
           />
         </MglGeoJsonSource>
+        <MglGeoJsonSource
+          v-if="selectedResponse"
+          source-id="selected-response"
+          :data="selectedResponse"
+        >
+          <MglCircleLayer
+            layer-id="selected-response"
+            :paint="{
+              'circle-stroke-color': 'red',
+              'circle-stroke-width': 2,
+              'circle-color': 'transparent',
+              'circle-radius': 20,
+            }"
+          />
+        </MglGeoJsonSource>
       </MglMap>
     </div>
     <PageTitle :title="callout.title" class="absolute top-8 left-8" />
-    <div v-if="selectedResponse" class="absolute left-0 bg-white p-4">
-      {{ selectedResponse.properties?.address.formatted_address }}
+    <div
+      v-if="selectedResponse"
+      class="absolute left-0 top-0 bottom-0 bg-white p-8 max-w-lg w-[40%] overflow-scroll shadow-lg"
+    >
+      <div class="callout-form-map">
+        <div
+          v-if="selectedPhotos.length > 0"
+          class="relative overflow-hidden mb-4"
+        >
+          {{ currentPhotoIndex }}
+          <ul
+            class="flex items-center transition-transform"
+            :style="{ transform: `translateX(${currentPhotoIndex * -100}%)` }"
+          >
+            <li
+              v-for="photo in selectedPhotos"
+              :key="photo.url"
+              class="w-full flex-none p-4"
+            >
+              <img class="w-full" :src="photo.url + '?w=600&h=600'" />
+            </li>
+          </ul>
+          <div
+            v-if="selectedPhotos.length > 1"
+            class="absolute top-1/2 inset-x-0 flex justify-between text-4xl font-bold transform -translate-y-1/2"
+          >
+            <div>
+              <button
+                class="bg-primary text-white w-12 h-12 rounded-full"
+                :class="currentPhotoIndex === 0 ? 'hidden' : ''"
+                @click="currentPhotoIndex--"
+              >
+                <font-awesome-icon :icon="faChevronLeft" />
+              </button>
+            </div>
+            <div>
+              <button
+                class="bg-primary text-white w-12 h-12 rounded-full"
+                :class="
+                  currentPhotoIndex === selectedPhotos.length - 1
+                    ? 'hidden'
+                    : ''
+                "
+                @click="currentPhotoIndex++"
+              >
+                <font-awesome-icon :icon="faChevronRight" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <Form
+          :key="selectedResponse.properties?.id"
+          :form="callout.formSchema"
+          :submission="{ data: selectedResponse.properties }"
+          :options="{ readOnly: true, noAlerts: true, renderMode: 'html' }"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -88,9 +158,14 @@ import {
 } from '../../../utils/api/api.interface';
 import { fetchCallout, fetchResponsesForMap } from '../../../utils/api/callout';
 import PageTitle from '../../../components/PageTitle.vue';
+import { Form } from 'vue-formio';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import 'vue-maplibre-gl/dist/vue-maplibre-gl.css';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const props = defineProps<{ id: string }>();
 
@@ -100,18 +175,13 @@ const router = useRouter();
 
 const hashPrefix = '#response-' as const;
 
+const titleProp = '';
+const photosProp = 'file';
+
 const callout = ref<GetCalloutDataWith<'form' | 'mapSchema'>>();
 const responses = ref<GetCalloutResponseMapData[]>([]);
 const center = ref<LngLatLike>([0, 0]);
 const zoom = ref(3);
-
-const selectedResponse = computed(
-  () =>
-    route.hash.startsWith(hashPrefix) &&
-    (responsesSource.value.features[+route.hash.slice(hashPrefix.length)] as
-      | GeoJSON.Feature<GeoJSON.Point>
-      | undefined)
-);
 
 const responsesSource = computed<GeoJSON.FeatureCollection>(() => ({
   type: 'FeatureCollection',
@@ -133,8 +203,30 @@ const responsesSource = computed<GeoJSON.FeatureCollection>(() => ({
   }),
 }));
 
+const selectedResponse = computed(
+  () =>
+    route.hash.startsWith(hashPrefix) &&
+    (responsesSource.value.features[+route.hash.slice(hashPrefix.length)] as
+      | GeoJSON.Feature<GeoJSON.Point>
+      | undefined)
+);
+
+const selectedPhotos = computed<any[]>(
+  () =>
+    (selectedResponse.value &&
+      selectedResponse.value.properties?.[photosProp]) ||
+    []
+);
+
+const currentPhotoIndex = ref(0);
+watch(selectedPhotos, () => {
+  currentPhotoIndex.value = 0;
+});
+
 // Zoom to a cluster or open a response
 function handleClick(e: { event: MapMouseEvent; map: Map }) {
+  if (!e.map.loaded()) return;
+
   const clusterFeatures = e.map.queryRenderedFeatures(e.event.point, {
     layers: ['clusters'],
   });
@@ -166,6 +258,8 @@ function handleClick(e: { event: MapMouseEvent; map: Map }) {
 
 // Add a cursor when hovering over a cluster or a point
 function handleMouseOver(e: { event: MapMouseEvent; map: Map }) {
+  if (!e.map.loaded()) return;
+
   const features = e.map.queryRenderedFeatures(e.event.point, {
     layers: ['clusters', 'unclustered-points'],
   });
@@ -196,3 +290,22 @@ onBeforeMount(async () => {
   responses.value = (await fetchResponsesForMap(props.id)).items;
 });
 </script>
+
+<style>
+.callout-form-map {
+  .form-group {
+    @apply mb-1;
+  }
+
+  .formio-component-file {
+    @apply hidden;
+  }
+
+  .col-form-label {
+    @apply float-left flex font-bold mr-2;
+    &::after {
+      content: ': ';
+    }
+  }
+}
+</style>
