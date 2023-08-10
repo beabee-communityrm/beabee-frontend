7320d9fc9c4b5667e18c790c03e6acb488e66fab
Will Franklin <will@willpf.co.uk>
2023-08-10 14:42:34 +0100

    Fix import

--- src/components/pages/admin/callouts/steps/ContentStep.vue
+++ src/components/pages/admin/callouts/steps/ContentStep.vue
@@ -38,7 +38,7 @@ import { ContentStepProps } from '../callouts.interface';
 import RichTextEditor from '../../../../rte/RichTextEditor.vue';
 import AppFormSection from '../../../../forms/AppFormSection.vue';
 import AppNotification from '../../../../AppNotification.vue';
-import FormBuilder from '../../../../form-builder/FormBuilder';
+import FormBuilder from '../../../../form-builder/FormBuilder.vue';
 
 const emit = defineEmits(['update:error', 'update:validated']);
 const props = defineProps<{
