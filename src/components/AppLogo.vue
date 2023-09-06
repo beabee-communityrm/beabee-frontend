868f4b6de764d6e7ec3ed6a59c43677d06aabb5b
Ricardo Lafuente <r@manufacturaindependente.org>
2023-09-06 11:07:00 +0100

    feat: add conditional link to sidebar logo

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,10 +1,12 @@
 <template>
-  <img
-    :src="generalContent.logoUrl || defaultLogo"
-    :alt="generalContent.organisationName"
-  />
+  <a :href="canAdmin ? '/admin' : '/profile'">
+    <img
+      :src="generalContent.logoUrl || defaultLogo"
+      :alt="generalContent.organisationName"
+    />
+  </a>
 </template>
 <script lang="ts" setup>
-import { generalContent } from '../store';
+import { canAdmin, generalContent } from '../store';
 import defaultLogo from '../assets/images/logo.png';
 </script>
