e9ec23b77210cc941111c4d3df0fa7a2182e3713
Will Franklin <will@willpf.co.uk>
2023-09-15 15:02:03 +0100

    Add translations for file component and other bits

--- src/components/form-renderer/FormRenderer.vue
+++ src/components/form-renderer/FormRenderer.vue
@@ -25,6 +25,8 @@ import {
   faRemove,
   faCamera,
 } from '@fortawesome/free-solid-svg-icons';
+import { generalContent } from '../../store';
+import { useI18n } from 'vue-i18n';
 
 defineEmits<{
   (e: 'submit', submission: FormSubmission): void;
@@ -37,6 +39,8 @@ const props = defineProps<{
   beforeSubmit?: (submission: FormSubmission) => boolean;
 }>();
 
+const { t } = useI18n();
+
 const formOpts = computed(() => ({
   readOnly: props.readonly,
   noAlerts: true,
@@ -48,6 +52,19 @@ const formOpts = computed(() => ({
     ) => {
       const canSubmit = !props.beforeSubmit || props.beforeSubmit(submission);
       next(canSubmit ? undefined : true);
+    },
+  },
+  language: generalContent.value.locale,
+  i18n: {
+    [generalContent.value.locale]: {
+      'Drop files to attach,': t('formRenderer.components.file.dropFiles'),
+      'use camera': t('formRenderer.components.file.useCamera'),
+      or: t('formRenderer.components.file.or'),
+      browse: t('formRenderer.components.file.browse'),
+      'Take Picture': t('formRenderer.components.file.takePicture'),
+      'Switch to file upload': t(
+        'formRenderer.components.file.switchToFileUpload'
+      ),
     },
   },
 }));
