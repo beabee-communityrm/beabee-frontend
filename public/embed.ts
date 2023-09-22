201d0043dd3f33864d9425e2e57faa66a96af825
Will Franklin <will@willpf.co.uk>
2023-09-22 12:29:32 +0100

    Allow embed script to cache

--- public/embed.ts
+++ public/embed.ts
@@ -3,7 +3,6 @@ import iframeResizer from 'iframe-resizer/js/iframeResizer';
 if (document.currentScript?.parentNode) {
   const id = 'beabee-embed-' + Math.random().toString(36).substring(7);
   const url = document.currentScript.getAttribute('data-url');
-  console.log(url);
 
   const iframe = document.createElement('iframe');
   iframe.setAttribute('src', '__appUrl__/' + url);
