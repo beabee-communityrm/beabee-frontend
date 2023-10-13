411bb7b2793ab38d57bb99eeb35f72583c8f25da
Will Franklin <will@willpf.co.uk>
2023-10-13 10:38:20 +0100

    Fix type

--- src/pages/callouts/[id]/map.vue
+++ src/pages/callouts/[id]/map.vue
@@ -365,22 +365,25 @@ async function handleAddClick(e: { event: MapMouseEvent; map: Map }) {
 
   const result = await reverseGeocode(coords.lat, coords.lng);
 
-  newResponseAnswers.value = {
-    [mapSchema.addressProp]: {
-      formatted_address: result?.formatted_address || '???',
-      features: result?.features || [],
-      geometry: {
-        // Use click location rather than geocode result
-        location: coords,
-      },
+  const address: GeocodeResult = {
+    formatted_address: result?.formatted_address || '???',
+    features: result?.features || [],
+    geometry: {
+      // Use click location rather than geocode result
+      location: coords,
     },
-    ...(mapSchema.addressPatternProp &&
-      result && {
-        [mapSchema.addressPatternProp]: formatGeocodeResult(
-          result,
-          mapSchema.addressPattern
-        ),
-      }),
+  };
+
+  const addressPattern =
+    mapSchema.addressPatternProp && result
+      ? formatGeocodeResult(result, mapSchema.addressPattern)
+      : undefined;
+
+  newResponseAnswers.value = {
+    [mapSchema.addressProp]: address,
+    ...(addressPattern && {
+      [mapSchema.addressPatternProp]: addressPattern,
+    }),
   };
 }
 
