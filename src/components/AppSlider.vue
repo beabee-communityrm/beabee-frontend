fe74b01a69c9993cce5cf07fc70a4639323de09d
Pascal Garber <pascal@artandcode.studio>
2023-10-19 18:46:18 +0200

    fix(AppSlider): Make the linter happy

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -16,40 +16,47 @@
 -->
 
 <template>
-  <div ref="slidesContainerEl" class="w-full overflow-x-hidden whitespace-nowrap flex flex-nowrap">
+  <div
+    ref="slidesContainerEl"
+    class="w-full overflow-x-hidden whitespace-nowrap flex flex-nowrap"
+  >
     <slot name="slides"></slot>
   </div>
-  <slot name="navigation" :prev-slide="prevSlide" :next-slide="nextSlide" :to-slide="toSlide"></slot>
-
+  <slot
+    name="navigation"
+    :prev-slide="prevSlide"
+    :next-slide="nextSlide"
+    :to-slide="toSlide"
+  ></slot>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 
 // TODO: this is a DOM type, but is not detected by Volar
-type ScrollBehavior = 'auto' | 'smooth'
+type ScrollBehavior = 'auto' | 'smooth';
 
 export interface AppSliderProps {
-  showNavigationButtons: boolean
-  infinite: boolean
+  showNavigationButtons: boolean;
+  infinite: boolean;
 }
 
 export interface AppSliderSlideEventDetails {
-  slideNumber: number
-  slideEl: HTMLElement
+  slideNumber: number;
+  slideEl: HTMLElement;
 }
 
 const emit = defineEmits<{
-  (e: 'slide', details: AppSliderSlideEventDetails): void
+  (e: 'slide', details: AppSliderSlideEventDetails): void;
 }>();
 
 const props = withDefaults(defineProps<AppSliderProps>(), {
   showNavigationButtons: false,
-  infinite: false
-})
+  infinite: false,
+});
 
-const activeSlide = ref(0)
-const slideCount = ref(0)
+const activeSlide = ref(0);
+const slideCount = ref(0);
 const slidesContainerEl = ref<HTMLElement | null>(null);
 const slideEls = ref<HTMLElement[]>([]);
 
@@ -62,55 +69,58 @@ const toSlide = (slideNumber: number, behavior: ScrollBehavior = 'smooth') => {
   const slideEl = slideEls.value[slideNumber];
 
   // Validate inputs
-  if(slideNumber < 0 || slideNumber >= slideCount.value) {
-    throw new Error(`slideNumber ${slideNumber} is out of bounds!`)
+  if (slideNumber < 0 || slideNumber >= slideCount.value) {
+    throw new Error(`slideNumber ${slideNumber} is out of bounds!`);
   }
-  if(!slidesContainerEl.value) throw new Error('slidesContainerEl is null!')
-  if(!slideEl) throw new Error(`slide element with index ${slideNumber} not found!`)
+  if (!slidesContainerEl.value) throw new Error('slidesContainerEl is null!');
+  if (!slideEl)
+    throw new Error(`slide element with index ${slideNumber} not found!`);
 
   // Scroll to the slide
   slideEl.scrollIntoView({ behavior, block: 'center' });
 
   // Finish up
-  activeSlide.value = slideNumber
+  activeSlide.value = slideNumber;
   emit('slide', {
     slideNumber,
-    slideEl
-  })
-}
+    slideEl,
+  });
+};
 
 /**
  * Go to the previous slide
  */
 const prevSlide = (behavior?: ScrollBehavior) => {
-  let prevIndex = activeSlide.value - 1
+  let prevIndex = activeSlide.value - 1;
   // If we're at the beginning, go to the end
-  if(prevIndex < 0) {
-    if(!props.infinite) return // If we're not infinite, don't do anything
-    prevIndex = slideCount.value - 1
+  if (prevIndex < 0) {
+    if (!props.infinite) return; // If we're not infinite, don't do anything
+    prevIndex = slideCount.value - 1;
   }
-  toSlide(prevIndex, behavior)
-}
+  toSlide(prevIndex, behavior);
+};
 
 /**
  * Go to the next slide
  */
 const nextSlide = (behavior?: ScrollBehavior) => {
-  let nextIndex = activeSlide.value + 1
+  let nextIndex = activeSlide.value + 1;
   // If we're at the end, go back to the beginning
-  if(nextIndex >= slideCount.value) {
-    if(!props.infinite) return // If we're not infinite, don't do anything
-    nextIndex = 0
+  if (nextIndex >= slideCount.value) {
+    if (!props.infinite) return; // If we're not infinite, don't do anything
+    nextIndex = 0;
   }
-  toSlide(nextIndex, behavior)
-}
+  toSlide(nextIndex, behavior);
+};
 
 const handleResize = () => {
-  toSlide(activeSlide.value, 'auto')
-}
+  toSlide(activeSlide.value, 'auto');
+};
 
 onMounted(() => {
-  slideEls.value.push(...Array.from(slidesContainerEl.value?.children || []) as HTMLElement[]);
+  slideEls.value.push(
+    ...(Array.from(slidesContainerEl.value?.children || []) as HTMLElement[])
+  );
   slideCount.value = slideEls.value.length;
   window.addEventListener('resize', handleResize);
 });
@@ -118,5 +128,4 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener('resize', handleResize);
 });
-
 </script>
