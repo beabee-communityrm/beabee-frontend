fa8b97bc9f9d55d0413fcdb3fd218d2e9fc6f928
Pascal Garber <pascal@artandcode.studio>
2023-10-19 18:19:28 +0200

    feat: Add new slider component

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,122 @@
+<!--
+  # AppSlider
+  A slider component that allows you to slide between elements. Useful for things like modals with multiple steps or a carousel.
+
+  ## Props
+  - `showNavigationButtons` (boolean): Whether or not to show the navigation buttons. Defaults to `false`.
+  - `infinite` (boolean): Whether or not to allow infinite scrolling. Defaults to `false`.
+
+  ## Events
+  - `slide` (details: AppSliderSlideEventDetails): Emitted when a slide is scrolled to. `details` contains the slide number and the slide element.
+
+  ## Slots
+  - `slides`: The slides to display in the slider.
+  - `navigation`: The navigation buttons to display. Receives the `prevSlide`, `nextSlide`, and `toSlide` functions as props.
+
+-->
+
+<template>
+  <div ref="slidesContainerEl" class="w-full overflow-x-hidden whitespace-nowrap flex flex-nowrap">
+    <slot name="slides"></slot>
+  </div>
+  <slot name="navigation" :prev-slide="prevSlide" :next-slide="nextSlide" :to-slide="toSlide"></slot>
+
+</template>
+
+<script lang="ts" setup>
+import { withDefaults, ref, onMounted, onUnmounted } from 'vue';
+
+// TODO: this is a DOM type, but is not detected by Volar
+type ScrollBehavior = 'auto' | 'smooth'
+
+export interface AppSliderProps {
+  showNavigationButtons: boolean
+  infinite: boolean
+}
+
+export interface AppSliderSlideEventDetails {
+  slideNumber: number
+  slideEl: HTMLElement
+}
+
+const emit = defineEmits<{
+  (e: 'slide', details: AppSliderSlideEventDetails): void
+}>();
+
+const props = withDefaults(defineProps<AppSliderProps>(), {
+  showNavigationButtons: false,
+  infinite: false
+})
+
+const activeSlide = ref(0)
+const slideCount = ref(0)
+const slidesContainerEl = ref<HTMLElement | null>(null);
+const slideEls = ref<HTMLElement[]>([]);
+
+/**
+ * Go to a specific slide
+ * @param slideNumber The slide number to go to
+ */
+const toSlide = (slideNumber: number, behavior: ScrollBehavior = 'smooth') => {
+  // Get the slide element
+  const slideEl = slideEls.value[slideNumber];
+
+  // Validate inputs
+  if(slideNumber < 0 || slideNumber >= slideCount.value) {
+    throw new Error(`slideNumber ${slideNumber} is out of bounds!`)
+  }
+  if(!slidesContainerEl.value) throw new Error('slidesContainerEl is null!')
+  if(!slideEl) throw new Error(`slide element with index ${slideNumber} not found!`)
+
+  // Scroll to the slide
+  slideEl.scrollIntoView({ behavior, block: 'center' });
+
+  // Finish up
+  activeSlide.value = slideNumber
+  emit('slide', {
+    slideNumber,
+    slideEl
+  })
+}
+
+/**
+ * Go to the previous slide
+ */
+const prevSlide = (behavior?: ScrollBehavior) => {
+  let prevIndex = activeSlide.value - 1
+  // If we're at the beginning, go to the end
+  if(prevIndex < 0) {
+    if(!props.infinite) return // If we're not infinite, don't do anything
+    prevIndex = slideCount.value - 1
+  }
+  toSlide(prevIndex, behavior)
+}
+
+/**
+ * Go to the next slide
+ */
+const nextSlide = (behavior?: ScrollBehavior) => {
+  let nextIndex = activeSlide.value + 1
+  // If we're at the end, go back to the beginning
+  if(nextIndex >= slideCount.value) {
+    if(!props.infinite) return // If we're not infinite, don't do anything
+    nextIndex = 0
+  }
+  toSlide(nextIndex, behavior)
+}
+
+const handleResize = () => {
+  toSlide(activeSlide.value, 'auto')
+}
+
+onMounted(() => {
+  slideEls.value.push(...Array.from(slidesContainerEl.value?.children || []) as HTMLElement[]);
+  slideCount.value = slideEls.value.length;
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+
+</script>
--- src/pages/_theme.vue
+++ src/pages/_theme.vue
@@ -5,59 +5,117 @@ meta:
   pageTitle: Theme settings
 </route>
 <template>
-  <h2 class="mb-2 font-title text-2xl">Colours</h2>
-  <div class="flex bg-white p-4">
-    <div class="flex-1">
-      <AppColor name="bg-primary" shade="Primary" />
-      <AppColor name="bg-primary-5" shade="5" />
-      <AppColor name="bg-primary-10" shade="10" />
-      <AppColor name="bg-primary-20" shade="20" />
-      <AppColor name="bg-primary-40" shade="40" />
-      <AppColor name="bg-primary-70" shade="70" />
-      <AppColor name="bg-primary-80" shade="80" />
+  <section class="my-5">
+    <h2 class="mb-2 font-title text-2xl">Colours</h2>
+    <div class="flex bg-white p-4">
+      <div class="flex-1">
+        <AppColor name="bg-primary" shade="Primary" />
+        <AppColor name="bg-primary-5" shade="5" />
+        <AppColor name="bg-primary-10" shade="10" />
+        <AppColor name="bg-primary-20" shade="20" />
+        <AppColor name="bg-primary-40" shade="40" />
+        <AppColor name="bg-primary-70" shade="70" />
+        <AppColor name="bg-primary-80" shade="80" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-body" shade="Body" />
+        <AppColor name="bg-body-40" shade="40" />
+        <AppColor name="bg-body-80" shade="80" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-link" shade="Link" />
+        <AppColor name="bg-link-10" shade="10" />
+        <AppColor name="bg-link-30" shade="30" />
+        <AppColor name="bg-link-70" shade="70" />
+        <AppColor name="bg-link-110" shade="110" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-warning" shade="Warning" />
+        <AppColor name="bg-warning-10" shade="10" />
+        <AppColor name="bg-warning-30" shade="30" />
+        <AppColor name="bg-warning-70" shade="70" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-success" shade="Success" />
+        <AppColor name="bg-success-10" shade="10" />
+        <AppColor name="bg-success-30" shade="30" />
+        <AppColor name="bg-success-70" shade="70" />
+        <AppColor name="bg-success-110" shade="110" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-danger" shade="Danger" />
+        <AppColor name="bg-danger-10" shade="10" />
+        <AppColor name="bg-danger-30" shade="30" />
+        <AppColor name="bg-danger-70" shade="70" />
+        <AppColor name="bg-danger-110" shade="110" />
+      </div>
+      <div class="flex-1">
+        <AppColor name="bg-grey" shade="Grey" />
+        <AppColor name="bg-grey-lighter" shade="Lighter" />
+        <AppColor name="bg-grey-light" shade="Light" />
+        <AppColor name="bg-grey-dark" shade="Dark" />
+        <AppColor name="bg-grey-darker" shade="Darker" />
+      </div>
     </div>
-    <div class="flex-1">
-      <AppColor name="bg-body" shade="Body" />
-      <AppColor name="bg-body-40" shade="40" />
-      <AppColor name="bg-body-80" shade="80" />
-    </div>
-    <div class="flex-1">
-      <AppColor name="bg-link" shade="Link" />
-      <AppColor name="bg-link-10" shade="10" />
-      <AppColor name="bg-link-30" shade="30" />
-      <AppColor name="bg-link-70" shade="70" />
-      <AppColor name="bg-link-110" shade="110" />
-    </div>
-    <div class="flex-1">
-      <AppColor name="bg-warning" shade="Warning" />
-      <AppColor name="bg-warning-10" shade="10" />
-      <AppColor name="bg-warning-30" shade="30" />
-      <AppColor name="bg-warning-70" shade="70" />
-    </div>
-    <div class="flex-1">
-      <AppColor name="bg-success" shade="Success" />
-      <AppColor name="bg-success-10" shade="10" />
-      <AppColor name="bg-success-30" shade="30" />
-      <AppColor name="bg-success-70" shade="70" />
-      <AppColor name="bg-success-110" shade="110" />
-    </div>
-    <div class="flex-1">
-      <AppColor name="bg-danger" shade="Danger" />
-      <AppColor name="bg-danger-10" shade="10" />
-      <AppColor name="bg-danger-30" shade="30" />
-      <AppColor name="bg-danger-70" shade="70" />
-      <AppColor name="bg-danger-110" shade="110" />
-    </div>
-    <div class="flex-1">
-      <AppColor name="bg-grey" shade="Grey" />
-      <AppColor name="bg-grey-lighter" shade="Lighter" />
-      <AppColor name="bg-grey-light" shade="Light" />
-      <AppColor name="bg-grey-dark" shade="Dark" />
-      <AppColor name="bg-grey-darker" shade="Darker" />
-    </div>
-  </div>
+  </section>
+
+  <section class="my-5">
+    <h2 class="mb-2 font-title text-2xl">Components</h2>
+
+    <section>
+      <h3 class="mb-2 font-title text-xl">AppSlider</h3>
+      <div class="bg-white p-4">
+        <div class="max-w-[1200px] mx-auto relative">
+          <AppSlider :show-navigation-buttons="true" :infinite="true">
+
+            <!-- Put your slides here -->
+            <template #slides>
+              <div class="flex-none w-full bg-danger">
+                <img class="mx-auto max-w-full" src="https://picsum.photos/seed/1/1200/300" alt="Lorem Picsum">
+              </div>
+              <div class="flex-none w-full bg-success">
+                <div class="w-full h-full flex justify-center items-center">
+                  <p>You can put anything you want in here!</p>
+                </div>
+              </div>
+              <div class="flex-none w-full bg-warning">
+                <img class="mx-auto max-w-full" src="https://picsum.photos/seed/2/1200/300" alt="Lorem Picsum">
+              </div>
+            </template>
+
+            <!-- Define your custom navigation template here -->
+            <template #navigation="{ nextSlide, prevSlide, toSlide }">
+              <span class="w-full h-full flex justify-between absolute top-0">
+                <button class="" @click="prevSlide()">
+                  <font-awesome-icon class="text-2xl p-2 text-white hover:text-link" :icon="PrevIcon" />
+                </button>
+                <button @click="nextSlide()">
+                  <font-awesome-icon class="text-2xl p-2 text-white hover:text-link" :icon="NextIcon" />
+                </button>
+              </span>
+
+              <button class="text-2xl p-2 text-white hover:text-link absolute top-0 left-0" @click="toSlide(0)">
+                <font-awesome-icon :icon="ResetIcon" />
+              </button>
+
+            </template>
+
+          </AppSlider>
+        </div>
+      </div>
+
+    </section>
+
+
+  </section>
 </template>
 
 <script lang="ts" setup>
 import AppColor from '../components/AppColor.vue';
+import AppSlider from '../components/AppSlider.vue';
+import {
+  faAngleRight as NextIcon,
+  faAngleLeft as PrevIcon,
+  faRotateBack as ResetIcon,
+} from '@fortawesome/free-solid-svg-icons';
 </script>
