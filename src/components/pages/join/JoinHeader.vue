f03c10a9f0a375a34898ce5370367c2262612633
Ana Isabel Carvalho <a@manufacturaindependente.org>
2023-11-02 15:20:27 +0000

    fix: removed subtitle prop from AppTitle and added subtitle in Account page.

--- src/components/pages/join/JoinHeader.vue
+++ src/components/pages/join/JoinHeader.vue
@@ -2,13 +2,12 @@
   <header class="mb-4 flex">
     <AppLogo class="w-16 flex-none self-start" />
     <div class="ml-4 flex flex-col self-center">
-      <h1 class="font-title text-2xl font-semibold">
-        {{ title }}
-      </h1>
+      <AppTitle>{{ title }}</AppTitle>
     </div>
   </header>
 </template>
 <script lang="ts" setup>
 import AppLogo from '../../AppLogo.vue';
+import AppTitle from '../../AppTitle.vue';
 defineProps<{ title: string }>();
 </script>
