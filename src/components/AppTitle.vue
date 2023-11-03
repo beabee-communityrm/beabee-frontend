c403ecb1c99aa553be32030735cf25fafaa251b2
Will Franklin <will@willpf.co.uk>
2023-11-03 12:46:02 +0000

    Simpler text-3.5xl

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,10 +1,7 @@
 <template>
   <h1
-    class="font-title tracking-tight mb-3"
-    :class="[
-      big ? 'text-3.5xl md:text-5xl' : 'text-3.5xl',
-      light ? 'font-light' : 'font-semibold',
-    ]"
+    class="font-title text-3.5xl tracking-tight mb-3"
+    :class="[big && 'md:text-5xl', light ? 'font-light' : 'font-semibold']"
   >
     <slot />
   </h1>
