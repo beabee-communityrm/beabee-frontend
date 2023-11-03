784d643046aa34a4da21f92115dcb3f4a79820d5
Will Franklin <will@willpf.co.uk>
2023-11-03 13:01:24 +0000

    No mb if last child

--- src/components/AppSubHeading.vue
+++ src/components/AppSubHeading.vue
@@ -1,5 +1,7 @@
 <template>
-  <h3 class="font-title text-1.8xl/tight font-semibold text-body-60 mb-3">
+  <h3
+    class="font-title text-1.8xl/tight font-semibold text-body-60 mb-3 last:mb-0"
+  >
     <slot />
   </h3>
 </template>
