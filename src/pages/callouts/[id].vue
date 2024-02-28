eaeb2a23b789005cccef7a0795e273dbf3c63fda
Will Franklin <will@willpf.co.uk>
2024-02-28 17:26:48 +0000

    Use lang query param if it exists

--- src/pages/callouts/[id].vue
+++ src/pages/callouts/[id].vue
@@ -3,13 +3,20 @@
 </template>
 <script lang="ts" setup>
 import { onBeforeMount, ref } from 'vue';
+import { useRoute } from 'vue-router';
 import { fetchCallout } from '@utils/api/callout';
 import type { GetCalloutDataWith } from '@type';
 
 const props = defineProps<{ id: string }>();
 const callout = ref<GetCalloutDataWith<'form' | 'responseViewSchema'>>();
 
+const route = useRoute();
+
 onBeforeMount(async () => {
-  callout.value = await fetchCallout(props.id, ['form', 'responseViewSchema']);
+  callout.value = await fetchCallout(
+    props.id,
+    ['form', 'responseViewSchema'],
+    route.query.lang ? route.query.lang.toString() : undefined
+  );
 });
 </script>
