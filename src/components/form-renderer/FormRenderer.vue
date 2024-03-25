e5a9669acab9a091eace14e40cf9b414e1dc397a
Will Franklin <will@willpf.co.uk>
2024-03-25 13:40:34 +0000

    Force form to reload when config changes

--- src/components/form-renderer/FormRenderer.vue
+++ src/components/form-renderer/FormRenderer.vue
@@ -1,5 +1,6 @@
 <template>
   <Form
+    :key="formOptsChanged"
     class="callout-form-renderer"
     :form="{ components }"
     :submission="modelValue && { data: modelValue }"
@@ -57,28 +58,32 @@ function handleChange(evt: FormChangeEvent, changes?: { noValidate: boolean }) {
   }
 }
 
-const formOpts = computed(() => ({
-  readOnly: props.readonly,
-  noAlerts: true,
-  renderMode: props.readonly ? 'html' : 'form',
-  i18n: {
-    custom: {
-      'Drop files to attach,': t('formRenderer.components.file.dropFiles'),
-      'use camera': t('formRenderer.components.file.useCamera'),
-      or: t('formRenderer.components.file.or'),
-      browse: t('formRenderer.components.file.browse'),
-      'Take Picture': t('formRenderer.components.file.takePicture'),
-      'Switch to file upload': t(
-        'formRenderer.components.file.switchToFileUpload'
-      ),
-      'Add Another': t('formRenderer.components.multiple.addAnother'),
-      required: t('form.errors.unknown.required'),
-      invalid_email: t('form.errors.unknown.email'),
-      invalid_url: t('form.errors.unknown.url'),
-      ...props.componentI18nText,
+const formOptsChanged = ref(0);
+const formOpts = computed(
+  () => ({
+    readOnly: props.readonly,
+    noAlerts: true,
+    renderMode: props.readonly ? 'html' : 'form',
+    i18n: {
+      custom: {
+        'Drop files to attach,': t('formRenderer.components.file.dropFiles'),
+        'use camera': t('formRenderer.components.file.useCamera'),
+        or: t('formRenderer.components.file.or'),
+        browse: t('formRenderer.components.file.browse'),
+        'Take Picture': t('formRenderer.components.file.takePicture'),
+        'Switch to file upload': t(
+          'formRenderer.components.file.switchToFileUpload'
+        ),
+        'Add Another': t('formRenderer.components.multiple.addAnother'),
+        required: t('form.errors.unknown.required'),
+        invalid_email: t('form.errors.unknown.email'),
+        invalid_url: t('form.errors.unknown.url'),
+        ...props.componentI18nText,
+      },
     },
-  },
-}));
+  }),
+  { onTrigger: () => formOptsChanged.value++ }
+);
 
 onBeforeMount(() => {
   library.add(
